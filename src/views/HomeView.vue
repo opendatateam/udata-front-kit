26bdda21be2a834905ab4135a3707b29b704f555
Alexandre Bulté <alexandre@bulte.net>
2023-06-19 15:56:17 +0200

    Make config.yaml available in app (#3)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
+<script setup>
+import config from "@/config.js"
+</script>
+
 <template>
   <main>
     <h1>Main in HomeView</h1>
+    Current config:
+    <pre>{{ config }}</pre>
   </main>
 </template>
