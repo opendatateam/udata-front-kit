b57d6ac158996204837dbfa932dab73ad258440e
Alexandre Bulté <alexandre@bulte.net>
2023-06-22 10:26:16 +0200

    Add datasets to store and org

--- src/services/api/resources/OrganizationsAPI.js
+++ src/services/api/resources/OrganizationsAPI.js
@@ -2,4 +2,15 @@ import DatagouvfrAPI from "../DatagouvfrAPI"
 
 export default class OrganizationsAPI extends DatagouvfrAPI {
   endpoint = "organizations"
+
+  /**
+   * Get datasets for an organization
+   *
+   * @param {str} org_id
+   * @returns {import("../../../composables/fetch").ComposableFetchResult}
+   */
+  getDatasets (org_id) {
+    const url = `${this.url()}/${org_id}/datasets/`
+    return this.makeRequestAndHandleResponse(url)
+  }
 }
