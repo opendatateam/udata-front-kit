622718063f3fe3b6a3593b1689890283ffe6d83d
Alexandre Bulté <alexandre@bulte.net>
2023-06-26 21:27:58 +0200

    Handle datasets pagination

--- src/store/index.js
+++ src/store/index.js
@@ -9,32 +9,67 @@ const api = new OrganizationsAPI()
 
 export const useDatasetStore = defineStore("dataset", {
   state: () => ({
-    data: []
+    data: {}
   }),
-  getters: {
-    // TODO: maybe move to actions since we have a param
-    datasetsForOrg: (state) => {
-      return (org_id) => {
-        return state.data.filter(d => d.organization.id === org_id || d.organization.slug === org_id)
-      }
-    }
-  },
   actions: {
-    getOrAddDatasetsForOrg (org_id) {
-      const existing = this.datasetsForOrg(org_id)
-      if (existing.length) return existing.value
-      // TODO: handle pagination
-      const { data, error } = api.getDatasets(org_id)
+    /**
+     * Get a datasets pagination object for a given org, from API infos
+     *
+     * @param {str} org_id
+     * @returns {Array<object>}
+     */
+    getDatasetsPaginationForOrg(org_id) {
+      const datasets = this.getDatasetsForOrg(org_id)
+      if (!datasets.data) return []
+      const nbPages = Math.ceil(datasets.total / datasets.page_size)
+      return [...Array(nbPages).keys()].map(page => {
+        page += 1
+        return {
+          label: page,
+          href: "#",
+          title: `Page ${page}`,
+        }
+      })
+    },
+    /**
+     * Get datasets in store for an org and a page
+     *
+     * @param {string} org_id
+     * @param {number} page
+     * @returns {object}
+     */
+    getDatasetsForOrg (org_id, page = 1) {
+      if (!this.data[org_id]) return {}
+      return this.data[org_id].find(d => d.page == page) || {}
+    },
+    /**
+     * Get from store or fetch from API datasets for an org and a page
+     *
+     * @param {string} org_id
+     * @param {number} page
+     * @returns {Array<object>}
+     */
+    getOrAddDatasetsForOrg (org_id, page = 1) {
+      const existing = this.getDatasetsForOrg(org_id, page)
+      if (existing.data) return existing
+      const { data, error } = api.getDatasets(org_id, page)
       // TODO: maybe move to async/await in API to avoid watchers (but will bubble up to component?)
       watch(data, (_data) => {
         if (_data.data) {
-          this.add(_data.data)
+          // store the full results with metadata
+          this.add(org_id, _data)
         }
       })
-      return this.datasetsForOrg(org_id)
+      return this.getDatasetsForOrg(org_id, page)
     },
-    add (datasets) {
-      datasets.forEach(d => this.data.push(d))
+    /**
+     * Store the result of a datasets fetch operation for an org in store
+     *
+     * @param {string} org_id
+     * @param {object} res
+     */
+    add (org_id, res) {
+      this.data[org_id] = [...(this.data[org_id] || []), res]
     },
   }
 })
@@ -45,23 +80,45 @@ export const useOrganizationStore = defineStore("organization", {
     data: []
   }),
   actions: {
-    fillFromConfig (withDatasets = false) {
+    /**
+     * Init the organization store from the config file values
+     *
+     * @returns {Array<object>}
+     */
+    fillFromConfig () {
       config.organizations.forEach((org_id) => {
-        this.getOrAdd(org_id, withDatasets)
+        this.getOrAdd(org_id)
       })
       return this.data
     },
+    /**
+     * Add an organization to the store
+     *
+     * @param {object} org
+     * @returns {object}
+     */
     add (org) {
       this.data.push(org)
       return org
     },
-    getOrAdd (org_id, withDatasets = false) {
+    /**
+     * Get an org from the store or from the API if not cached
+     *
+     * @param {string} org_id
+     * @returns {object}
+     */
+    getOrAdd (org_id) {
       const existing = this.data.find(o => o.value.id === org_id || o.value.slug === org_id)
       if (existing) return existing.value
       const { data } = api.get(org_id)
-      if (withDatasets) this.getOrAddDatasetForOrg(org_id)
       return this.add(data)
     },
+    /**
+     * Get datasets for an org from the store or from the API ()
+     *
+     * @param {str} org_id
+     * @returns {object}
+     */
     getOrAddDatasetForOrg (org_id) {
       const dStore = useDatasetStore()
       return dStore.getOrAddDatasetsForOrg(org_id)
--- src/views/organizations/OrganizationDetailView.vue
+++ src/views/organizations/OrganizationDetailView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useRoute } from "vue-router"
-import { computed } from "vue"
+import { computed, ref } from "vue"
 
 import { useOrganizationStore, useDatasetStore } from "../../store"
 
@@ -10,9 +10,15 @@ const organizationId = route.params.oid
 const orgStore = useOrganizationStore()
 const datasetStore = useDatasetStore()
 
-const fetchDatasets = true
-const org = orgStore.getOrAdd(organizationId, fetchDatasets)
-const datasets = computed(() => datasetStore.datasetsForOrg(organizationId))
+const org = orgStore.getOrAdd(organizationId)
+
+const currentPage = ref(1)
+const pages = computed(() => datasetStore.getDatasetsPaginationForOrg(organizationId))
+const datasets = computed(() => datasetStore.getOrAddDatasetsForOrg(organizationId, currentPage.value))
+
+function onUpdatePage (page) {
+  currentPage.value = page + 1
+}
 </script>
 
 <template>
@@ -20,6 +26,7 @@ const datasets = computed(() => datasetStore.datasetsForOrg(organizationId))
   <div>{{ org.description }}</div>
   <h2>Jeux de données</h2>
   <ul>
-    <li v-for="d in datasets">{{ d.title }}</li>
+    <li v-for="d in datasets.data">{{ d.title }}</li>
   </ul>
+  <DsfrPagination v-if="pages.length" :current-page="currentPage - 1" :pages="pages" @update:current-page="onUpdatePage" />
 </template>
