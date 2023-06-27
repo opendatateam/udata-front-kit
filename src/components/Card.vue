b2f2018216ef1cce11aaa54f789a8963b09f898e
Alexandre Bulté <alexandre@bulte.net>
2023-06-27 16:48:09 +0200

    Add dataset cards (#11)

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,18 @@
 <script setup>
-defineProps(["title", "description", "img", "link", "altImg"])
+import { computed } from 'vue'
+
+const props = defineProps(["title", "description", "img", "link", "altImg", "type"])
+
+const typeClass = computed(() => !!props.type && `es-card--${props.type}`)
 </script>
 
 <template>
-  <div class="fr-card fr-enlarge-link">
+  <div :class="['fr-card', 'fr-enlarge-link', typeClass]">
     <div class="fr-card__body">
       <div class="fr-card__content">
-        <h3 class="fr-card__title">
+        <h6 class="fr-card__title">
           <router-link :to="link">{{ title }}</router-link>
-        </h3>
+        </h6>
         <p class="fr-card__desc">
           <text-clamp v-if="description" :auto-resize="true" :text="description" :max-lines="2" />
         </p>
@@ -28,6 +32,15 @@ defineProps(["title", "description", "img", "link", "altImg"])
 </template>
 
 <style scoped lang="scss">
+.es-card--dataset {
+  .fr-card__header{
+    width: 20%;
+    flex: 0 0 20%;
+  }
+  h6 {
+    font-size: 1em;
+  }
+}
 .fr-card {
   &__img {
     img {
