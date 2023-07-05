fb57980cd8a6893a30087c2521bb9fdfb6484c25
Alexandre Bulté <alexandre@bulte.net>
2023-07-05 15:34:01 +0200

    Add discussions for a dataset

---
+++ src/store/DiscussionStore.js
@@ -0,0 +1,64 @@
+import { defineStore } from "pinia"
+import DiscussionsAPI from "../services/api/resources/DiscussionsAPI"
+
+const discussionsAPI = new DiscussionsAPI()
+
+export const useDiscussionStore = defineStore("discussion", {
+  state: () => ({
+    data: {},
+  }),
+  actions: {
+    /**
+     * Get discussions for a dataset from store
+     *
+     * @param {str} dataset_id
+     * @returns {Array<object>}
+     */
+    getDiscussionsForDataset (dataset_id, page = 1) {
+      if (!this.data[dataset_id]) return {}
+      return this.data[dataset_id].find(d => d.page == page) || {}
+    },
+    /**
+     * Async function to trigger API fetch of discussions for a dataset
+     *
+     * @param {string} dataset_id
+     * @returns {Array<object>}
+     */
+    async loadDiscussionsForDataset (dataset_id, page = 1) {
+      const existing = this.getDiscussionsForDataset(dataset_id, page)
+      if (existing.data) return existing
+      const discussions = await discussionsAPI.getDiscussions(dataset_id, page)
+      this.addDiscussions(dataset_id, discussions)
+      return this.getDiscussionsForDataset(dataset_id, page)
+    },
+    /**
+     * Store the result of a discussions fetch operation for a dataset in store
+     *
+     * @param {string} dataset_id
+     * @param {Array<object>} res
+     */
+    addDiscussions (dataset_id, res) {
+      if (!res) return
+      this.data[dataset_id] = [...(this.data[dataset_id] || []), res]
+    },
+    /**
+     * Get a discussions pagination object for a given dataset, from store infos
+     *
+     * @param {str} dataset_id
+     * @returns {Array<object>}
+     */
+    getDiscussionsPaginationForDataset (dataset_id) {
+      const discussions = this.getDiscussionsForDataset(dataset_id)
+      if (!discussions.data) return []
+      const nbPages = Math.ceil(discussions.total / discussions.page_size)
+      return [...Array(nbPages).keys()].map(page => {
+        page += 1
+        return {
+          label: page,
+          href: "#",
+          title: `Page ${page}`,
+        }
+      })
+    },
+  },
+})
--- src/views/datasets/DatasetDetailView.vue
+++ src/views/datasets/DatasetDetailView.vue
@@ -7,6 +7,7 @@ import { useRoute } from "vue-router"
 
 import { useDatasetStore } from "../../store/DatasetStore"
 import { useReuseStore } from "../../store/ReuseStore"
+import { useDiscussionStore } from "../../store/DiscussionStore"
 import Card from "../../components/Card.vue"
 
 const route = useRoute()
@@ -14,9 +15,14 @@ const datasetId = route.params.did
 
 const datasetStore = useDatasetStore()
 const reuseStore = useReuseStore()
+const discussionStore = useDiscussionStore()
 
 const dataset = computed(() => datasetStore.get(datasetId) || {})
+const discussionsPages = ref([])
 const reuses = ref([])
+const discussions = ref({})
+const discussionsPage = ref(1)
+const expandedDiscussion = ref(null)
 
 const formatFileSize = (fileSize) => {
   if (!fileSize) return "Taille inconnue"
@@ -40,9 +46,25 @@ const description = computed(() => {
   }
 })
 
-// launch reuses fetch as soon as we have the technical id
+const onUpdatePage = (page) => {
+  discussionsPage.value = page + 1
+  discussionStore.loadDiscussionsForDataset(dataset.value.id, discussionsPage.value).then((d) => discussions.value = d)
+}
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString)
+  return new Intl.DateTimeFormat("default", {dateStyle: "full", timeStyle: "short"}).format(date)
+}
+
+// launch reuses and discussions fetch as soon as we have the technical id
 watch(dataset, _dataset => {
-  if (_dataset?.id) reuseStore.loadReusesForDataset(_dataset.id).then((r) => reuses.value = r)
+  if (_dataset?.id) {
+    reuseStore.loadReusesForDataset(_dataset.id).then((r) => reuses.value = r)
+    discussionStore.loadDiscussionsForDataset(_dataset.id).then((d) => {
+      discussions.value = d
+      discussionsPages.value = discussionStore.getDiscussionsPaginationForDataset(_dataset.id)
+    })
+  }
 })
 
 onMounted(() => {
@@ -72,4 +94,41 @@ onMounted(() => {
       :img="r.organization?.logo || r.owner.avatar"
     />
   </div>
+
+  <h2 class="fr-mt-4w">Discussions</h2>
+  <div v-if="!discussions.data?.length">Pas de discussion pour ce jeu de données.</div>
+
+  <DsfrAccordionsGroup>
+    <li v-for="discussion in discussions.data">
+      <DsfrAccordion :id="discussion.id" :title="discussion.title" :expanded-id="expandedDiscussion" @expand="id => expandedDiscussion = id">
+        <template #default>
+          <ul class="es__comment__container">
+            <li v-for="comment in discussion.discussion">
+              <div class="es__comment__metadata fr-mb-1v">
+                <span class="es__comment__author">{{ comment.posted_by.first_name }} {{ comment.posted_by.last_name }}</span>
+                <span class="es__comment__date fr-ml-1v">le {{ formatDate(comment.posted_on) }}</span>
+              </div>
+              <div class="es__comment__content">{{ comment.content }}</div>
+            </li>
+          </ul>
+        </template>
+      </DsfrAccordion>
+    </li>
+  </DsfrAccordionsGroup>
+  <DsfrPagination class="fr-mt-2w" v-if="discussionsPages.length" :current-page="discussionsPage - 1" :pages="discussionsPages" @update:current-page="onUpdatePage" />
 </template>
+
+<style scoped lang="scss">
+ul.es__comment__container {
+  list-style-type: none;
+  padding-inline-start: 0.25rem;
+  li {
+    padding-bottom: 1.5rem;
+  }
+}
+.es__comment__metadata {
+  .es__comment__author {
+    font-weight: bold;
+  }
+}
+</style>
