5469e407e4fb7b692808cc4e27ef79a2fa89fce5
Alexandre Bulté <alexandre@bulte.net>
2023-07-06 09:26:35 +0200

    Filter archived datasets

--- src/services/api/resources/DatasetsAPI.js
+++ src/services/api/resources/DatasetsAPI.js
@@ -2,4 +2,17 @@ import DatagouvfrAPI from "../DatagouvfrAPI"
 
 export default class DatasetsAPI extends DatagouvfrAPI {
   endpoint = "datasets"
+
+  /**
+   * Get datasets for an organization
+   *
+   * @param {str} org_id Technical org id
+   * @returns {object}
+   */
+  async getDatasetsForOrganization (org_id, page = 1) {
+    // WARNING: specify `-created` or another sort explicitely because default sort has a pagination issue
+    const url = `${this.url()}/?organization=${org_id}&page=${page}&sort=-created`
+    return await this.makeRequestAndHandleResponse(url)
+  }
+
 }
--- src/services/api/resources/OrganizationsAPI.js
+++ src/services/api/resources/OrganizationsAPI.js
@@ -2,15 +2,4 @@ import DatagouvfrAPI from "../DatagouvfrAPI"
 
 export default class OrganizationsAPI extends DatagouvfrAPI {
   endpoint = "organizations"
-
-  /**
-   * Get datasets for an organization
-   *
-   * @param {str} org_id
-   * @returns {object}
-   */
-  async getDatasets (org_id, page = 1) {
-    const url = `${this.url()}/${org_id}/datasets/?page=${page}`
-    return await this.makeRequestAndHandleResponse(url)
-  }
 }
