56046b25d2e306aed914fd61d00be21b7ec9aac7
Alexandre Bulté <alexandre@bulte.net>
2023-10-11 14:51:28 +0200

    Dynamic config based on CONFIG_NAME

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ViteYaml from '@modyfi/vite-plugin-yaml'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    ViteYaml(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@root': resolve(__dirname, './'),
-    }
-  },
-  base: '/',
+export default defineConfig(({ mode })=> {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      vue(),
+      ViteYaml(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@root': resolve(__dirname, './'),
+        '@siteConfig': fileURLToPath(new URL(`./configs/${env.CONFIG_NAME}`, import.meta.url))
+      }
+    },
+    base: '/',
+  }
 })
