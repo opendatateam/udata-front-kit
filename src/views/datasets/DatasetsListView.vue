7070941dcc6c2700bea29c269bc293925dfe1116
Alexandre Bulté <alexandre@bulte.net>
2023-10-12 15:17:56 +0200

    Refactor DatasetsListView in depth

--- src/views/datasets/DatasetsListView.vue
+++ src/views/datasets/DatasetsListView.vue
@@ -1,42 +1,37 @@
 <script setup>
-import { computed, ref, watchEffect } from "vue"
+import { computed, onMounted, ref, watchEffect } from "vue"
 import { useRoute, onBeforeRouteUpdate } from "vue-router"
 import { useSearchStore } from "../../store/SearchStore"
+import { useTopicStore } from "../../store/TopicStore"
 import Tile from "../../components/Tile.vue"
 import config from "@/config"
 
 const route = useRoute()
 const store = useSearchStore()
 const query = computed(() => route.query.q)
-const topic = computed(() => route.query.topic)
-// TODO: this should be handled as reactive value instead
-let searchTopicId = topic.value
-const searchTopicName = ref("")
 const currentPage = ref(1)
 
-const subTopics = config.website.list_highlighted_topics
-
-const ALL_DATA_LABEL = "Toutes les données"
+const topicStore = useTopicStore()
+const topic = computed(() => route.query.topic)
+const selectedTopicId = ref(null)
 
 const datasets = computed(() => store.datasets)
 const pages = computed(() => store.pagination)
 
-// TODO: this should be a filtered reactive subset of TopicStore
-// storing both the id and the name
-const topicFacetNames = (config.website.list_highlighted_topics?.length)
-  ?  [ALL_DATA_LABEL].concat(config.website.list_highlighted_topics.map((item) => item.name))
-  : null
+const topicsConf = config.website.list_highlighted_topics
+const topicOptions = computed(() => {
+  if (!topicsConf?.length) return
+  const topics = topicStore.$state.data.filter(t => {
+    return topicsConf.map(st => st.id).includes(t.id)
+  }).map(t => {
+    return { value: t.id, text: t.name }
+  })
+  return [{ value: "", text: "Toutes les données" }, ...topics]
+})
 
-const onSelectTopic = (value) => {
-  let newTopic = null
-  if (value != ALL_DATA_LABEL) {
-    newTopic = config.website.list_highlighted_topics.filter(
-      (item) => item.name === value
-    )[0].id
-  }
+const onSelectTopic = (topicId) => {
+  selectedTopicId.value = topicId
   currentPage.value = 1
-  searchTopicId = newTopic
-  store.search(query.value, searchTopicId, currentPage.value)
 }
 
 // reset currentPage when query changes
@@ -44,15 +39,21 @@ onBeforeRouteUpdate((to, from) => {
   currentPage.value = 1
 })
 
+onMounted(() => {
+  if (topicsConf?.length) {
+    topicStore.loadTopicsFromList(topicsConf)
+  }
+})
+
 // fill topic name when arriving on the page with a topic ID
-// TODO: topic id is not synced in the URL when selecting a topic
+// TODO: topicId is not updated when selecting a topic
 watchEffect(() => {
-  if (!topic.value || !config.website.list_highlighted_topics) return
-  searchTopicName.value = config.website.list_highlighted_topics.find(t => t.id === topic.value).name
+  if (!topic.value || !topicsConf) return
+  selectedTopicId.value = topic.value
 })
 
 watchEffect(() => {
-  store.search(query.value, searchTopicId, currentPage.value)
+  store.search(query.value, selectedTopicId.value, currentPage.value)
 })
 </script>
 
@@ -64,17 +65,17 @@ watchEffect(() => {
       Aucun résultat pour cette recherche.
     </div>
     <div class="fr-grid-row">
-      <div v-if="topicFacetNames" class="fr-col-md-3 fr-pr-md-2w fr-mb-2w">
+      <div v-if="topicsConf" class="fr-col-md-3 fr-pr-md-2w fr-mb-2w">
         <DsfrSelect
-          :model-value="searchTopicName"
-          :options="topicFacetNames"
+          :model-value="selectedTopicId"
+          :options="topicOptions"
+          default-unselected-text="Toutes les données"
           @update:modelValue="onSelectTopic"
-          v-if="subTopics"
         >
           <template #label>Thématiques</template>
         </DsfrSelect>
       </div>
-      <div :class="[topicFacetNames ? 'fr-col-md-9' : 'fr-col-md-12']">
+      <div :class="[topicsConf ? 'fr-col-md-9' : 'fr-col-md-12']">
         <ul class="fr-grid-row fr-grid-row--gutters es__tiles__list">
           <li v-for="d in datasets" class="fr-col-12 fr-col-lg-4">
             <Tile
