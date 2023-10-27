31fbb1019516b9cebf770149f7d1aaf9478e1e93
Marvin SAURAYE <msauraye@veepee.com>
2023-10-27 08:30:55 +0200

    Add temporary search modal

--- src/views/bouquets/BouquetEditView.vue
+++ src/views/bouquets/BouquetEditView.vue
@@ -1,11 +1,16 @@
 <script setup>
-import { ref, onMounted, computed, watch } from 'vue'
+import { ref, onMounted, computed } from 'vue'
+import { useDatasetStore } from '@/store/DatasetStore'
+import { useTopicStore } from '@/store/TopicStore'
+import SearchAPI from '../../services/api/SearchAPI'
 import { useRouter, useRoute } from 'vue-router'
 import Tooltip from '@/components/Tooltip.vue'
-import config from '@/config'
 import { getSubThemes } from '@/utils'
-import { useTopicStore } from '@/store/TopicStore'
+import Multiselect from '@vueform/multiselect'
+import config from '@/config'
 
+const searchAPI = new SearchAPI()
+const datasetStore = useDatasetStore()
 const topicStore = useTopicStore()
 const router = useRouter()
 const route = useRoute()
@@ -13,7 +18,12 @@ const route = useRoute()
 const isCreate = route.name === 'bouquet_add'
 
 const form = ref({})
+const datasets = ref([])
+const selectedDataset = ref({})
+const selector = ref(null)
+const isEditDesc = ref(false)
 const loadedBouquet = ref({})
+const isModalOpen = ref(false)
 const isFormValidated = ref(false)
 const errorMessage = ref()
 const steps = [
@@ -51,6 +61,70 @@ const validateAndMoveToStep = (newStep) => {
   }
 }
 
+const modalActions = [
+  {
+    label: 'Valider',
+    onClick: (event) => {
+      event.preventDefault()
+      onSubmitModal()
+      isModalOpen.value = false
+    }
+  },
+  {
+    label: 'Annuler',
+    secondary: true,
+    onClick: (event) => {
+      event.preventDefault()
+      isModalOpen.value = false
+      selectedDataset.value = {}
+    }
+  }
+]
+
+const search = async (query) => {
+  if (!query) return []
+  const results = await searchAPI._search(query, { page_size: 10 })
+  return results.data.map(r => {
+    return { value: r.id, label: r.title }
+  }).filter(r => !datasets.value.map(d => d.dataset.id).includes(r.value))
+}
+
+const onSubmitModal = async () => {
+  if (!selectedDataset.value.id) return
+  if (isEditDesc.value) {
+    const idx = datasets.value.findIndex(d => d.dataset.id === selectedDataset.value.id)
+    datasets.value[idx].description = selectedDataset.value.description
+  } else {
+    const dataset = await datasetStore.load(selectedDataset.value.id)
+    if (dataset) {
+      datasets.value.push({ dataset, description: selectedDataset.value.description })
+    }
+  }
+  selectedDataset.value = {}
+  isEditDesc.value = false
+}
+
+const onDeleteDataset = (datasetId) => {
+  datasets.value = datasets.value.filter(d => d.dataset.id !== datasetId)
+  delete loadedBouquet.value.extras[`${config.universe_name}:${datasetId}:description`]
+}
+
+const onEditDataset = (dataset) => {
+  isModalOpen.value = true
+  isEditDesc.value = true
+  selectedDataset.value.id = dataset.id
+  selectedDataset.value.title = dataset.title
+  selectedDataset.value.description = datasets.value.find(d => d.dataset.id === dataset.id).description
+}
+
+const extrasFromDatasets = () => {
+    const extras = {}
+    for (const dataset of datasets.value) {
+      extras[`${config.universe_name}:${dataset.dataset.id}:description`] = dataset.description
+    }
+    return extras
+  }
+
 const onSubmit = async () => {
   let res
   const data = {
@@ -68,13 +142,13 @@ const onSubmit = async () => {
     res = await topicStore.create({
       ...data,
       tags: [config.universe.name],
-      extras: extras
+      extras: { ...extras, ...extrasFromDatasets() }
     })
   } else {
     res = await topicStore.update(loadedBouquet.value.id, {
       ...data,
       tags: loadedBouquet.value.tags,
-      extras: { ...loadedBouquet.value.extras, ...extras}
+      extras: { ...loadedBouquet.value.extras, ...extras, ...extrasFromDatasets()}
     })
   }
 
@@ -87,12 +161,23 @@ const onSubmit = async () => {
   }
 }
 
+const loadDatasets = async (datasetIds, bouquet) => {
+    for (const datasetId of datasetIds) {
+      const dataset = await datasetStore.load(datasetId)
+      datasets.value.push({
+        dataset,
+        description: bouquet.extras[`${config.universe_name}:${dataset.id}:description`] || "",
+      })
+    }
+  }
+
 onMounted(() => {
   if (!isCreate) {
     topicStore.load(route.params.bid).then((data) => {
       loadedBouquet.value = data
       form.value.name = data.name
-      form.value.description = data.description
+      form.value.description = data.description,
+      loadDatasets(data.datasets.map(d => d.id), data)
     })
   }
 })
@@ -179,6 +264,83 @@ onMounted(() => {
               </div>
             </div>
           </div>
+
+          <!-- TODO: REMOVE SEEARCH AFTER TEST -->
+          <DsfrButton label="Ajouter un jeu de données"
+            @click.stop.prevent="isModalOpen = true"
+            ref="modalOrigin" :secondary="true" />
+          <DsfrModal
+            ref="modal"
+            :opened="isModalOpen"
+            title="Ajouter un jeu de données"
+            @close="isModalOpen = false"
+            :origin="$refs.modalOrigin"
+            :actions="modalActions"
+          >
+            <label for="select-datasets">Sélectionnez un jeu de données</label>
+            <Multiselect
+              noOptionsText="Précisez ou élargissez votre recherche"
+              ref="selector"
+              class="fr-mt-1w fr-mb-2w"
+              placeholder="Cherchez un jeu de données"
+              name="select-datasets"
+              v-model="selectedDataset.id"
+              :filter-results="false"
+              :min-chars="1"
+              :resolve-on-load="false"
+              :delay="0"
+              :searchable="true"
+              :options="search"
+              v-if="!isEditDesc"
+            />
+            <DsfrInput v-else
+              class="fr-mt-1w fr-mb-2w"
+              :disabled="true"
+              v-model="selectedDataset.title"
+            />
+            <DsfrInput
+              class="fr-mt-1w fr-mb-2w"
+              v-model="selectedDataset.description"
+              label="Description du jeu de données dans le bouquet"
+              placeholder="Pourquoi avez-vous ajouté ce jeu de données ?"
+              :label-visible="true"
+              :is-textarea="true"
+            />
+          </DsfrModal>
+          <div class="fr-table">
+            <table v-if="datasets.length">
+              <thead>
+                <tr>
+                  <th scope="col">Titre</th>
+                  <th scope="col">Organisation</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="d in datasets">
+                  <td>{{ d.dataset.title }}</td>
+                  <td>{{ d.dataset.organization?.name }}</td>
+                  <td>{{ d.description }}</td>
+                  <td>
+                    <div class="es__button-container">
+                      <DsfrButton
+                        :icon-only="true" icon="ri-delete-bin-line" size="sm"
+                        title="Supprimer"
+                        @click.stop.prevent="onDeleteDataset(d.dataset.id)"
+                      />
+                      <DsfrButton
+                        :icon-only="true" icon="ri-pencil-line" size="sm"
+                        title="Editer"
+                        @click.stop.prevent="onEditDataset(d.dataset)"
+                      />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!-- END -->
           <DsfrButton type="submit" class="fr-mt-2w" label="Suivant" />
       </div>
     </form>
