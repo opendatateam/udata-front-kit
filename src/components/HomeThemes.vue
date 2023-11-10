0e297036c727fa5e2c37cf56cc0cee43fd6104db
Erica Delagnier <erica.delagnier@multi.coop>
2023-11-10 11:24:08 +0100

    create tile list component

---
+++ src/components/HomeThemes.vue
@@ -0,0 +1,83 @@
+<template>
+    <div v-if="selectedTheme === null">
+        <ul class="fr-grid-row fr-grid-row--gutters es__tiles__list fr-mt-1w">
+            <li v-for="theme in themeList" class="fr-col-12 fr-col-lg-4">
+                <Tile
+                    :style="getCustomBoxShadow(theme.color)"
+                    :link="`/?theme=${theme.name}`"
+                    :title="theme.name"
+                    :description="getThemeDescription(theme)"
+                />
+            </li>
+        </ul>
+    </div>
+    <div v-else>
+        <DsfrBreadcrumb class="home-selection-breadcrumb" :links="breadcrumbList" />
+        <ul class="fr-grid-row fr-grid-row--gutters es__tiles__list fr-mt-1w">
+            <li v-for="subtheme in selectedTheme.subthemes" class="fr-col-12 fr-col-lg-4">
+                <Tile
+                    :style="getCustomBoxShadow(selectedTheme.color)"
+                    :link="goToTopicList(subtheme)"
+                    :title="subtheme.name"
+                />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    import Tile from './Tile.vue'
+    import config from '../config'
+    import type { Theme, Subtheme } from '../model'
+
+    export default {
+        name: "HomeThemes",
+        components: {
+            Tile: Tile,
+        },
+        props: {
+            selectedThemeName: {
+                type: String
+            }
+        },
+        computed: {
+            selectedTheme() {
+                for (const theme of this.themeList) {
+                    if (theme.name === this.selectedThemeName) {
+                        return theme
+                    }
+                }
+                return null
+            },
+            themeList() {
+                return config.themes
+            },
+            breadcrumbList() {
+                return [
+                    { text: 'Accueil', to: '/'},
+                    { text: this.selectedTheme.name }
+                ]
+            }
+        },
+        methods: {
+            getCustomBoxShadow(color : string) {
+                return `box-shadow: rgb(221, 221, 221) 0px 0px 0px 1px inset, #${color} 0px -4px 0px 0px inset`
+            },
+            goToTopicList(subtheme: Subtheme): string {
+                return "link to topic list" // TO DO redirect to search result once the page is created
+            },
+            getThemeDescription(theme: Theme) : string {
+                const nbSubthemes = theme.subthemes.length
+                switch (nbSubthemes) {
+                    case 0:
+                        return "Pas de chantiers"
+                    case 1:
+                        return "1 chantier"
+                    default :
+                        return nbSubthemes + " chantiers"
+                }
+            }
+
+        }
+    }
+</script>
---
+++ src/model.ts
@@ -0,0 +1,11 @@
+interface Theme {
+    name: string,
+    color: string,
+    subthemes: Subtheme[]
+}
+
+interface Subtheme {
+    name: string
+}
+
+export type {Theme, Subtheme}
