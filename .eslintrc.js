de0f727367073bedb405c62d5393277838a47b52
Mauko Quiroga <public@mauko.me>
2023-11-17 17:19:45 +0100

    build(npm): add ts to husky (#162)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,13 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/vue3-recommended',
     '@vue/eslint-config-standard',
-    'plugin:json/recommended',
-    'eslint:recommended', 
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'standard',
-    'prettier'
+    'plugin:json/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+    'standard'
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
