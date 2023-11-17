1a77c6e2bc186f319b6f425cfe82db78f76e14ee
Mauko Quiroga <public@mauko.me>
2023-11-17 19:39:44 +0100

    build(npm): fix linter & formatter config (#162)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,29 @@ module.exports = {
     node: true
   },
   extends: [
-    '@vue/eslint-config-standard',
-    'plugin:json/recommended',
     'plugin:vue/vue3-recommended',
-    'prettier',
+    'plugin:json/recommended',
+    '@vue/eslint-config-standard',
     'standard',
-    'standard-with-typescript'
+    'prettier'
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@babel/eslint-parser'
-  }
+  },
+  overrides: [
+    {
+      extends: [
+        '@vue/eslint-config-typescript',
+        'standard-with-typescript',
+        'prettier'
+      ],
+      files: ['*.ts'],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: __dirname
+      }
+    }
+  ]
 }
