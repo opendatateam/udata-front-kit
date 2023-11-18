0a87eb2ea6e0d0f7663f309bcc104265a4fe5513
Mauko Quiroga <public@mauko.me>
2023-11-18 18:04:23 +0100

    chore(doc): update readme (#92)

---
+++ src/utils/hocs.ts
@@ -0,0 +1,36 @@
+/*
+ * @fileoverview A collection of higher-order components (HOCs) that can be
+ * used to wrap components to provide additional functionality.
+ */
+import { toast } from 'vue3-toastify'
+import { useLoading } from 'vue-loading-overlay'
+
+type fn<T> = (...args: any[]) => Promise<T>
+
+interface Loader {
+  hide: () => void
+}
+
+function withLoading<T>(fn: fn<T>): fn<T> {
+  return async function (...args: any[]): Promise<T> {
+    const loader: Loader = useLoading().show()
+
+    return fn(...args)
+      .then((response: T) => response)
+      .catch((error) => error)
+      .finally(() => loader.hide())
+  }
+}
+
+function withToaster<T>(fn: fn<T>): fn<T> {
+  return async function (...args: any[]): Promise<T> {
+    return fn(...args)
+      .then((response: T) => response)
+      .catch((error) => {
+        toast(error.message, { type: 'error', autoClose: false })
+        return error
+      })
+  }
+}
+
+export { withLoading, withToaster }
