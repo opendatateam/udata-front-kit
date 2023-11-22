7e894b77e8149bd2e5bf8d591677554900b3c148
Marvin SAURAYE <msauraye@veepee.com>
2023-11-22 17:34:58 +0100

    Change url for dataset eco

--- src/views/bouquets/BouquetEditView.vue
+++ src/views/bouquets/BouquetEditView.vue
@@ -108,6 +108,11 @@ const availabilityEnum = {
   URL_AVAILABLE: 'url available'
 }
 
+const goToDatasetPage = (id) => {
+  const url = config.website.menu_items.find((link) => link.linkPage === '/datasets')
+  return `${url.linkPage}/${id}`
+}
+
 const addDatasetsPropertiesToExtras = async () => {
   const setUri = urlData.value ? urlData.value : null
   if (title.value && description.value) {
@@ -122,7 +127,7 @@ const addDatasetsPropertiesToExtras = async () => {
         datasetsProperties.value[datasetsPropertiesKey].push({
           title: title.value,
           description: description.value,
-          uri: dataset.page,
+          uri: goToDatasetPage(selectedDataset.value.id),
           id: selectedDataset.value.id,
           available: availabilityEnum.ECO_AVAILABLE
         })
@@ -643,6 +648,7 @@ onMounted(() => {
             >
               <DsfrAccordion
                 :title="datasetProperties.title"
+                :id="datasetProperties.id"
                 :expanded-id="datasetProperties.id"
                 @expand="datasetProperties.id = $event"
               >
