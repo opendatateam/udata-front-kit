22bcbf079586e72196d88553dafad1a223bb716c
Alexandre Bulté <alexandre@bulte.net>
2023-12-05 15:12:49 +0100

    feat(page): SimplePage from embedded md file

--- src/store/PageStore.ts
+++ src/store/PageStore.ts
@@ -1,16 +1,21 @@
 import { defineStore } from 'pinia'
 
+export interface RootState {
+  data: string
+}
+
 export const pageStore = defineStore('getPage', {
-  state: () => ({
-    data: ''
-  }),
+  state: () =>
+    ({
+      data: ''
+    } satisfies RootState),
   getters: {
     content: (state) => {
-      return state.data || ''
+      return state.data
     }
   },
   actions: {
-    async getPageFromUrl(url) {
+    async getPageFromLocation(url: string) {
       const response = await fetch(url)
       const res = await response.text()
       this.data = res
