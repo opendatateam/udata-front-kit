37a97e82c3c2da21defc7002f05825824ed13739
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2023-12-11 16:43:07 +0100

    feat: add custom footer with data.gouv.fr style

---
+++ public/meteo-france/pages/about.md
@@ -0,0 +1,51 @@
+# ***À propos de meteo.data.gouv.fr***
+
+### **Qu’est-ce que [meteo.data.gouv.fr](http://meteo.data.gouv.fr/) ?**
+
+[meteo.data.gouv.fr](http://meteo.data.gouv.fr/) est une plateforme qui centralise et structure les données publiques relatives à la météo et au climat.
+
+### **À qui s’adresse [meteo.data.gouv.fr](http://meteo.data.gouv.fr/) ?**
+
+[meteo.data.gouv.fr](http://meteo.data.gouv.fr/) s’adresse aux réutilisateurs de données cherchant des données brutes téléchargeables et utilisables de manière libre et gratuite, qu'ils soient de l’administration, du secteur privé ou de la société civile.
+
+### **Les données ouvertes : qu’est-ce que c’est ?**
+
+Les données ouvertes (open data) sont des données en accès libre, gratuit et facilement réutilisables par toutes et tous. Ces données peuvent être produites par l’administration, mais aussi par des acteurs privés ou encore des citoyens.
+
+### **Quelles données sont mises à disposition ?**
+
+Lancée en décembre 2023, la plateforme a vocation à être enrichie de nouvelles données au fur et à mesure et de mettre en avant des données relatives à la météo et au climat.
+
+Les données déjà disponibles : 
+
+- Données climatologiques de base : données climatologiques de toutes les stations françaises depuis leur ouverture, pour tous les paramètres disponibles.
+- Données climatologiques de référence pour le changement climatique : données « mémoire du climat » qui permettent de constater les effets déjà observés du changement climatique.
+
+Les données suivantes seront bientôt disponibles : 
+
+- Données d’observation mesurées par les stations météorologiques ;
+- Alertes météorologiques ;
+- Données radar ;
+- Modèles de données de prévision météorologique numérique (PNT).
+
+### Une api sera t-elle disponible ?
+
+Les API de Météo-France disponible sur [ce portail](https://portail-api.meteofrance.fr/devportal/apis) seront bientôt référencées directement sur [meteo.data.gouv.fr](http://meteo.data.gouv.fr). 
+
+### **Un énième portail open data ?**
+
+[meteo.data.gouv.fr](http://meteo.data.gouv.fr/) n’est pas un nouveau portail open data. C’est une exposition thématique du catalogue de [data.gouv.fr](http://data.gouv.fr/) sur le modèle de [transport.data.gouv.fr](http://transport.data.gouv.fr/), par exemple. Toutes les données disponibles ici le sont dans les mêmes conditions sur [data.gouv.fr](http://data.gouv.fr/).
+
+### **Qui est derrière [meteo.data.gouv.fr](http://meteo.data.gouv.fr/) ?**
+
+Cette plateforme est le fruit d’une collaboration entre l’équipe [data.gouv.fr](http://data.gouv.fr/) de la Direction Interministérielle du Numérique et Météo-France.
+
+Celle-ci s’inscrit dans une démarche plus large de construction de biens communs numériques facilitant la création de plateformes thématiques exposant de la donnée publique.
+
+### Un retour, une question ?
+
+**[Vos retours](https://tally.so/r/wQ7y47)** sont bienvenus pour nous permettre d’améliorer la plateforme !
+
+Si vous avez une question qui porte sur les données vous pouvez *vous adresser à Météo-France*.
+
+Si vous avez une question spécifique sur le fonctionnement de la plateforme vous pouvez [écrire à l’équipe data.gouv.fr.](https://support.data.gouv.fr/)
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,201 @@
+<script lang="ts" setup>
+import {
+  computed,
+  onMounted,
+  onUnmounted,
+  ref,
+  useSlots,
+  type StyleValue
+} from 'vue'
+
+type DsfrFooterProps = {
+  serviceLogoSrc?: string
+  logoText?: string | string[]
+  operatorImgSrc?: string
+  operatorImgStyle?: StyleValue
+  footerPhrase: string
+  footerExternalLinks: Array<{ name: string; url: string }>
+  footerMandatoryLinks: Array<{ name: string; url: string }>
+}
+
+const props = withDefaults(defineProps<DsfrFooterProps>(), {
+  logoText: () => 'Gouvernement',
+  operatorImgAlt: '',
+  operatorImgSrc: '',
+  operatorImgStyle: () => ({}),
+  serviceLogoSrc: '',
+  footerPhrase: '',
+  footerExternalLinks: [],
+  footerMandatoryLinks: []
+})
+
+const slots = useSlots()
+const isWithSlotOperator = computed(
+  () => Boolean(slots.operator?.().length) || !!props.operatorImgSrc
+)
+
+const goToPage = (page) => {
+  window.location.href = page
+}
+</script>
+
+<template>
+  <footer>
+    <div class="fr-container">
+      <div class="footer-top">
+        <div>
+          <div class="footer-logo">
+            <div class="footer-logo-child">
+              <DsfrLogo :logo-text="logoText" data-testid="header-logo" />
+            </div>
+            <div class="footer-logo-child" v-if="isWithSlotOperator">
+              <!-- @slot Slot nommé operator pour le logo opérateur. Sera dans `<div class="fr-header__operator">` -->
+              <slot name="operator">
+                <img
+                  v-if="operatorImgSrc"
+                  class="fr-responsive-img"
+                  :src="operatorImgSrc"
+                  :alt="operatorImgAlt"
+                  :style="operatorImgStyle"
+                />
+              </slot>
+            </div>
+            <div class="footer-logo-child">
+              <img
+                class="fr-responsive-img"
+                :src="serviceLogoSrc"
+                alt=""
+                style="
+                  height: 35px;
+                  vertical-align: middle;
+                  margin-right: 0.75em;
+                  width: auto;
+                "
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="footer-phrase-and-links">
+            <div>
+              <p class="footer-phrase">{{ footerPhrase }}</p>
+            </div>
+            <div class="external-links">
+              <div
+                class="external-link"
+                v-for="item in footerExternalLinks"
+                v-bind:key="item.name"
+                @click="goToPage(item.url)"
+              >
+                {{ item.name }}
+                <span
+                  class="fr-icon-external-link-line"
+                  aria-hidden="true"
+                ></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <div class="fr-container">
+        <div class="mandatory-links">
+          <div
+            class="mandatory-link"
+            v-for="item in footerMandatoryLinks"
+            v-bind:key="item.name"
+            @click="goToPage(item.url)"
+          >
+            {{ item.name }}
+          </div>
+        </div>
+        <br />
+        <div class="bottom-phrase">
+          Sauf mention contraire, tous les contenus de ce site sous sous
+          <a
+            href="https://github.com/etalab/licence-ouverte/blob/master/LO.md#licence-ouverte-20open-licence-20"
+            >Licence Ouverte 2.0</a
+          >
+          <span class="fr-icon-external-link-line" aria-hidden="true"></span>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+@media (min-width: 1248px) {
+  .footer-top {
+    display: flex;
+  }
+}
+@media (max-width: 800px) {
+  .mandatory-links {
+    height: 60px;
+  }
+}
+
+footer {
+  margin-top: 100px;
+  border-top: 2px solid #050091;
+  padding-top: 30px;
+}
+.footer-logo {
+  display: flex;
+}
+
+.footer-logo-child {
+  margin-right: 50px;
+  line-height: 80px;
+}
+
+.footer-phrase {
+  font-size: 14px;
+}
+
+.external-links {
+  display: flex;
+}
+
+.external-link {
+  font-weight: bold;
+  margin-right: 10px;
+  font-size: 14px;
+}
+
+.external-link:hover {
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+.footer-bottom {
+  margin-top: 30px;
+  border-top: 1px solid #dddddd;
+}
+
+.mandatory-links {
+  margin-top: 20px;
+  display: flex;
+}
+
+.mandatory-link {
+  font-size: 12px;
+  padding-right: 20px;
+  border-right: 1px solid #ddd;
+  display: inline-block;
+  height: 20px;
+  margin-right: 20px;
+  color: #666666;
+}
+
+.mandatory-link:hover {
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+.bottom-phrase {
+  font-size: 12px;
+  padding-bottom: 30px;
+}
+</style>
