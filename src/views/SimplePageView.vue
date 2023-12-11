9d746f155fb5e2c2cb1a48bdbb2bd243a224e466
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2023-12-11 15:23:04 +0100

    feat: added breadcrumbs for about page

--- src/views/SimplePageView.vue
+++ src/views/SimplePageView.vue
@@ -1,12 +1,14 @@
 <script setup>
 import MarkdownIt from 'markdown-it'
 import { computed, watchEffect } from 'vue'
+import { useRouter } from 'vue-router'
 
 import { pageStore } from '../store/PageStore'
 
 const markdown = new MarkdownIt()
 const store = pageStore()
 const content = computed(() => store.content)
+const title = useRouter().currentRoute.value.meta.title
 const props = defineProps({
   url: {
     type: String,
@@ -14,13 +16,19 @@ const props = defineProps({
   }
 })
 
+const links = computed(() => [{ to: '/', text: 'Accueil' }, { text: title }])
+
 watchEffect(async () => {
   if (!props.url) return
   store.getPageFromUrl(props.url)
+  console.log(title)
 })
 </script>
 
 <template>
+  <div class="fr-container">
+    <DsfrBreadcrumb :links="links" />
+  </div>
   <div class="fr-container fr-mt-4w fr-mb-4w">
     <!-- eslint-disable-next-line vue/no-v-html -->
     <div v-html="markdown.render(content)" />
