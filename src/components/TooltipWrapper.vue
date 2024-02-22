63d8b75c12991ce99081a8b9d7c9faa1e3f86c1a
Alexandre Bulté <alexandre@bulte.net>
2024-02-22 18:02:38 +0100

    Remove title from tooltip component

--- src/components/TooltipWrapper.vue
+++ src/components/TooltipWrapper.vue
@@ -3,7 +3,7 @@ import 'tippy.js/dist/tippy.css'
 import 'tippy.js/themes/light.css'
 import { Tippy } from 'vue-tippy'
 
-const props = defineProps({
+defineProps({
   title: {
     type: String,
     required: true
@@ -11,28 +11,20 @@ const props = defineProps({
   text: {
     type: String,
     required: true
-  },
-  optional: {
-    type: Boolean,
-    default: false
   }
 })
 </script>
 
 <template>
-  <div class="container">
-    {{ title }}
-    <span v-if="!props.optional" class="required">&nbsp;*</span>
-    <tippy theme="light">
-      <VIcon
-        name="ri-question-fill"
-        color="var(--blue-france-sun-113-625)"
-        style="margin-left: 0.2em"
-      />
-      <template #content>
-        <!-- eslint-disable-next-line vue/no-v-html -->
-        <span v-html="text"></span>
-      </template>
-    </tippy>
-  </div>
+  <tippy theme="light">
+    <VIcon
+      name="ri-question-fill"
+      color="var(--blue-france-sun-113-625)"
+      style="margin-left: 0.2em"
+    />
+    <template #content>
+      <!-- eslint-disable-next-line vue/no-v-html -->
+      <span v-html="text"></span>
+    </template>
+  </tippy>
 </template>
--- src/custom/ecospheres/components/forms/dataset/DatasetPropertiesForm.vue
+++ src/custom/ecospheres/components/forms/dataset/DatasetPropertiesForm.vue
@@ -10,15 +10,19 @@
         <input id="label" v-model="title" class="fr-input" type="text" />
       </div>
       <div class="fr-mt-1w fr-mb-4w">
-        <Tooltip
-          title="Raison d'utilisation dans ce bouquet"
-          text="Ajoutez ici l'ensemble des informations nécessaires à la compréhension, l'objectif et l'utilisation du bouquet. N'hésitez pas à indiquer la réglementation ou une documentation liée au bouquet."
-        />
-        <Tooltip
-          title="Utilisez du markdown pour mettre en forme votre texte"
-          text="* simple astérisque pour italique *<br/> ** double astérisque pour gras **<br/> # un dièse pour titre 1<br/> ## deux dièses pour titre 2<br/> *  astérisque pour une liste<br/> lien : [[https://exemple.fr]]"
-          :optional="true"
-        />
+        <div class="container">
+          Raison d'utilisation dans ce bouquet
+          <span class="required">&nbsp;*</span>
+          <Tooltip
+            text="Ajoutez ici l'ensemble des informations nécessaires à la compréhension, l'objectif et l'utilisation du bouquet. N'hésitez pas à indiquer la réglementation ou une documentation liée au bouquet."
+          />
+        </div>
+        <div class="container">
+          Utilisez du markdown pour mettre en forme votre texte
+          <Tooltip
+            text="* simple astérisque pour italique *<br/> ** double astérisque pour gras **<br/> # un dièse pour titre 1<br/> ## deux dièses pour titre 2<br/> *  astérisque pour une liste<br/> lien : [[https://exemple.fr]]"
+          />
+        </div>
         <textarea id="purpose" v-model="purpose" class="fr-input" type="text" />
       </div>
       <div class="fr-mt-1w fr-mb-4w">
