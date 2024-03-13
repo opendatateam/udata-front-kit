51ec51fc2f6e6a5baf81460213427b6749f4eba3
Alexandre Bulté <alexandre@bulte.net>
2024-03-13 15:04:59 +0100

    feat: custom ecospheres homepage

--- src/components/HomeThemes.vue
+++ src/components/HomeThemes.vue
@@ -1,37 +1,21 @@
 <template>
-  <div v-if="selectedTheme === null" className="without_breadcrumb">
-    <ul class="fr-grid-row fr-grid-row--gutters es__tiles__list fr-mt-1w">
+  <div class="fr-container--fluid">
+    <ul
+      class="fr-grid-row fr-grid-row--gutters es__tiles__list home-themes-tiles"
+    >
       <li
         v-for="theme in themeList"
         :key="theme.name"
-        class="fr-col-12 fr-col-lg-4"
+        class="fr-col-12 fr-col-lg-3"
       >
         <Tile
           :style="getCustomBoxShadow(theme.color)"
-          :link="`/?theme=${theme.name}`"
+          :link="{ name: 'bouquets', query: { theme: theme.name } }"
           :title="theme.name"
           :description="getThemeDescription(theme)"
         />
       </li>
     </ul>
-  </div>
-  <div v-else>
-    <DsfrBreadcrumb class="breadcrumb" :links="breadcrumbList" />
-    <div class="with_breadcrumb">
-      <ul class="fr-grid-row fr-grid-row--gutters es__tiles__list fr-mt-1w">
-        <li
-          v-for="subtheme in selectedTheme.subthemes"
-          :key="subtheme.name"
-          class="fr-col-12 fr-col-lg-4"
-        >
-          <Tile
-            :style="getCustomBoxShadow(selectedTheme.color)"
-            :link="goToTopicList(subtheme)"
-            :title="subtheme.name"
-          />
-        </li>
-      </ul>
-    </div>
   </div>
 </template>
 
@@ -45,26 +29,9 @@ export default {
   components: {
     Tile
   },
-  props: {
-    selectedThemeName: {
-      type: String,
-      default: ''
-    }
-  },
   computed: {
-    selectedTheme() {
-      for (const theme of this.themeList) {
-        if (theme.name === this.selectedThemeName) {
-          return theme
-        }
-      }
-      return null
-    },
     themeList() {
       return config.themes
-    },
-    breadcrumbList() {
-      return [{ text: 'Accueil', to: '/' }, { text: this.selectedTheme.name }]
     }
   },
   methods: {
@@ -89,17 +56,10 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
-.without_breadcrumb {
-  margin: 128px 0 0;
-}
-
-.with_breadcrumb {
-  margin: 60px 0 0;
-}
-
-.breadcrumb {
-  margin-top: 40px;
-  text-align: left;
+<style lang="scss">
+.home-themes-tiles {
+  .fr-tile__body {
+    align-items: flex-start;
+  }
 }
 </style>
--- src/custom/ecospheres/views/HomeView.vue
+++ src/custom/ecospheres/views/HomeView.vue
@@ -1,3 +1,104 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+import HomeThemes from '@/components/HomeThemes.vue'
+import config from '@/config'
+
+const query = ref('')
+const router = useRouter()
+
+const doSearch = () => {
+  router.push({ name: 'datasets', query: { q: query.value } })
+}
+
+const homepageTitle = config.website.homepage_title
+const searchConfig = config.website.search_bar
+</script>
+
 <template>
-  <h1>Home ES</h1>
+  <section class="fr-container fr-py-12v">
+    <div class="fr-grid-row fr-grid-row--gutters">
+      <div class="fr-col">
+        <h1>{{ homepageTitle }}</h1>
+        <p>
+          <i>ecologie</i><strong>.data.gouv</strong><i>.fr</i> est un catalogue
+          de données qui centralise les données de la transition écologique.
+        </p>
+        <p>
+          Il vise à vous aider à trouver les données utiles au déploiement de
+          politiques publiques portées par le ministère de la transition
+          écologique et de la cohésion des territoires.
+        </p>
+      </div>
+      <div class="fr-col-md-6 fr-hidden fr-unhidden-md fr-grid-row--center">
+        <img style="max-width: 100%" src="../assets/home-global-schema.svg" />
+      </div>
+    </div>
+  </section>
+  <section class="fr-container--fluid bouquets">
+    <div class="fr-container fr-py-12v">
+      <div class="fr-grid-row fr-grid-row--gutters">
+        <div class="fr-col">
+          <h2>Bouquets de données</h2>
+          <p>
+            Pour trouver les bonnes données, utiles à la mise en œuvre des
+            politiques publiques environnementales, les données sont rassemblées
+            sous forme de bouquets.
+          </p>
+          <p>
+            En constante évolution, le catalogue sera progressivement enrichi de
+            nouveaux bouquets de données.
+          </p>
+          <DsfrButton secondary>
+            <a
+              href="https://ecospheres.gitbook.io/doc/"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Consulter la documentation</a
+            >
+          </DsfrButton>
+        </div>
+        <div class="fr-col-md-6 fr-hidden fr-unhidden-md fr-grid-row--center">
+          <img
+            style="max-width: 100%"
+            src="../assets/home-bouquets-schema.svg"
+          />
+        </div>
+      </div>
+      <div class="fr-mt-10v">
+        <h6>Trouvez un bouquet en entrant par thématique</h6>
+        <HomeThemes v-if="config.themes" />
+      </div>
+    </div>
+  </section>
+  <section v-if="searchConfig.display" class="fr-container fr-py-12v">
+    <h2>Jeux de données</h2>
+    <p>
+      Parcourez tous les jeux de données présents sur <i>ecologie</i
+      ><strong>.data.gouv</strong><i>.fr</i>.
+    </p>
+    <p>
+      Au fur et à mesure de son enrichissement, le catalogue moissonnera de
+      nouvelles données.
+    </p>
+    <div class="fr-grid-row fr-grid-row--gutters">
+      <div class="fr-col-12 fr-col-md-8">
+        <DsfrSearchBar
+          v-model="query"
+          button-text="Rechercher"
+          class="search-bar-input"
+          :placeholder="searchConfig.placeholder"
+          :large="true"
+          @search="doSearch"
+        />
+      </div>
+    </div>
+  </section>
 </template>
+
+<style scoped lang="scss">
+.bouquets {
+  background-color: #f6f6f6;
+}
+</style>
