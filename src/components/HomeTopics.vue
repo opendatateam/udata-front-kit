d5c999aa15a88a930123d1bb82681b9bef97b47c
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2024-03-25 13:29:31 +0100

    config

--- src/components/HomeTopics.vue
+++ src/components/HomeTopics.vue
@@ -1,18 +1,24 @@
 <script setup>
 import { onMounted, computed } from 'vue'
 
+import config from '@/config'
+
+import TopicDataCard from '../components/topics/TopicDataCard.vue'
+import TopicDataTile from '../components/topics/TopicDataTile.vue'
 import { useTopicStore } from '../store/TopicStore'
 
 const topicStore = useTopicStore()
 
 const props = defineProps(['topics'])
 const topics = props.topics
+const typeHighlightedTopics = config.website.type_highlighted_topics
 
 const topicsData = computed(() => topicStore.data)
 
 onMounted(() => {
   topicStore.loadTopicsFromList(topics)
 })
+const urlHighlightedTopics = config.website.url_highlighted_topics
 
 const goToPage = (page) => {
   window.location.href = page
@@ -20,43 +26,18 @@ const goToPage = (page) => {
 </script>
 
 <template>
-  <div v-if="topicsData.length" class="datagouv-components fr-mt-5w">
-    <h2 class="subtitle subtitle--uppercase text-align-left">Thématiques</h2>
-    <div class="fr-grid-row fr-grid-row--gutters">
-      <div v-for="topic in topicsData" class="fr-col-12 fr-col-md-6">
-        <div
-          class="topic-card"
-          @click="goToPage('/datasets?topic=' + topic.id)"
-        >
-          <div class="topic-title">{{ topic.name }}</div>
-          <div class="topic-description">{{ topic.description }}</div>
-        </div>
-      </div>
+  <div>
+    <div v-if="typeHighlightedTopics == 'card'">
+      <TopicDataCard
+        :topicsData="topicsData"
+        :urlHighlightedTopics="urlHighlightedTopics"
+      />
+    </div>
+    <div v-if="typeHighlightedTopics == 'tile'">
+      <TopicDataTile
+        :topicsData="topicsData"
+        :urlHighlightedTopics="urlHighlightedTopics"
+      />
     </div>
   </div>
 </template>
-
-<style scoped>
-.section-title {
-  text-align: left;
-  font-size: 20px;
-}
-.topic-card {
-  height: 100%;
-  border: 1px solid #ebebeb;
-  padding: 20px;
-  text-align: left;
-  min-height: 190px;
-  border-bottom: 3px solid #3557a2;
-}
-.topic-title {
-  font-size: 18px;
-  font-weight: bold;
-  margin-bottom: 20px;
-  min-height: 50px;
-}
-.topic-card:hover {
-  cursor: pointer;
-  background-color: #ebebeb;
-}
-</style>
