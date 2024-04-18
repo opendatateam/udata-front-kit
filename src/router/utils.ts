d7de0f4a5ded9d1f7c6b245dca94a6a2acbeddb0
Alexandre Bulté <alexandre@bulte.net>
2024-04-18 09:40:24 +0200

    useless util

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -5,11 +5,6 @@ interface RouteLocationParamsAsString
   params: Record<string, string>
 }
 
-interface RouteLocationQueryAsString
-  extends Omit<RouteLocationNormalizedLoaded, 'query'> {
-  query: Record<string, string>
-}
-
 /**
  * Exposes first element from route params that could contain an array
  * Warning: this will discard the other values if any
@@ -23,19 +18,4 @@ export const useRouteParamsAsString = (): RouteLocationParamsAsString => {
     ])
   )
   return { ...route, params }
-}
-
-/**
- * Exposes first element from route query that could contain an array
- * Warning: this will discard the other values if any
- */
-export const useRouteQueryAsString = (): RouteLocationQueryAsString => {
-  const route = useRoute()
-  const query = Object.fromEntries(
-    Object.entries(route.query).map(([key, value]) => [
-      key,
-      Array.isArray(value) ? String(value[0]) : String(value)
-    ])
-  )
-  return { ...route, query }
 }
