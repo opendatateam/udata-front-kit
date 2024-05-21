5bbbf8d52d05b6c2dbe115cddbbb2c61301212b7
Alexandre Bulté <alexandre@bulte.net>
2024-05-21 17:10:10 +0200

    feat(dataset): add extended information panel

---
+++ src/components/datasets/ExtendedInformationPanel.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { ExtendedDatasetV2 } from '@/model/dataset'
+
+const props = defineProps({
+  dataset: {
+    type: Object as () => ExtendedDatasetV2,
+    required: true
+  }
+})
+
+const contactPoint = props.dataset.contact_point
+const accessRights = props.dataset.extras?.harvest?.['dct:accessRights']
+
+const hasExtendedInfo = !!accessRights || !!contactPoint
+</script>
+
+<template>
+  <div
+    v-if="hasExtendedInfo"
+    class="fr-py-3w fr-mb-3w border-bottom border-default-grey"
+  >
+    <h2 class="subtitle subtitle--uppercase">Informations étendues</h2>
+    <div class="fr-text--sm fr-m-0">
+      <div class="fr-grid-row fr-grid-row--gutters">
+        <div v-if="accessRights" class="fr-col-12 fr-col-sm-6 fr-col-md-4">
+          <h3 class="subtitle fr-mb-1v">Conditions d'accès et d'utilisation</h3>
+          <p class="fr-text--sm fr-m-0 text-mention-grey">
+            {{ accessRights }}
+          </p>
+        </div>
+        <div v-if="contactPoint" class="fr-col-12 fr-col-sm-6 fr-col-md-4">
+          <h3 class="subtitle fr-mb-1v">Point de contact</h3>
+          <p class="fr-text--sm fr-m-0 text-mention-grey">
+            <a :href="`mailto:${contactPoint.email}`">{{
+              contactPoint.name
+            }}</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
