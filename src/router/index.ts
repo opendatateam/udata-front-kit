53a4d4b16b353e027b59a559ef374fabb4fd1334
nico <contact@nardu.in>
2024-10-08 18:14:05 +0200

    revert auth requirement for bouquet add route

--- src/router/index.ts
+++ src/router/index.ts
@@ -109,7 +109,7 @@ const defaultRoutes: RouteRecordRaw[] = [
     path: `/admin/${topicsSlug}/add`,
     name: `${topicsSlug}_add`,
     component: async () => await import('@/views/bouquets/BouquetFormView.vue'),
-    meta: { requiresAuth: false },
+    meta: { requiresAuth: true },
     props: { isCreate: true }
   },
   {
