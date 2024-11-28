fbbb792757a6e7f5dc58025012951c6e005531c5
Tarik Amar <tarik.amar@gmail.com>
2024-11-28 13:57:33 +0100

    Wording

--- src/views/datasets/DatasetDetailView.vue
+++ src/views/datasets/DatasetDetailView.vue
@@ -236,9 +236,7 @@ watch(
           </div>
         </div>
         <template v-if="dataset.harvest">
-          <h2 class="subtitle fr-mt-3v fr-mb-1v">
-            Publication du jeu de données
-          </h2>
+          <h2 class="subtitle fr-mt-3v fr-mb-1v">Publication</h2>
           <p>
             {{
               dataset.harvest.created_at
@@ -247,9 +245,7 @@ watch(
             }}
           </p>
 
-          <h2 class="subtitle fr-mt-3v fr-mb-1v">
-            Dernière révision du jeu de données
-          </h2>
+          <h2 class="subtitle fr-mt-3v fr-mb-1v">Dernière révision</h2>
           <p>
             {{
               dataset.harvest.modified_at
@@ -259,12 +255,10 @@ watch(
           </p>
         </template>
         <template v-else>
-          <h2 class="subtitle fr-mt-3v fr-mb-1v">Création du jeu de données</h2>
+          <h2 class="subtitle fr-mt-3v fr-mb-1v">Création</h2>
           <p>{{ formatDate(dataset.created_at) }}</p>
 
-          <h2 class="subtitle fr-mt-3v fr-mb-1v">
-            Dernière mise à jour du jeu de données
-          </h2>
+          <h2 class="subtitle fr-mt-3v fr-mb-1v">Dernière mise à jour</h2>
           <p>{{ formatDate(dataset.last_update) }}</p>
         </template>
         <div v-if="license">
