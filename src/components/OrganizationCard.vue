2e26c9af11fda9922dc2fb801523021237a4a3fc
Tarik Amar <tarik.amar@multi.coop>
2024-11-29 10:11:30 +0100

    All card clickable

--- src/components/OrganizationCard.vue
+++ src/components/OrganizationCard.vue
@@ -20,60 +20,60 @@ const isPublicService = (): boolean =>
 </script>
 
 <template>
-  <div class="fr-tile-v2 border">
-    <div class="fr-grid-row fr-grid-row--middle fr-mb-8v">
-      <div class="fr-col-auto">
-        <div
-          v-if="organization.logo"
-          class="fr-tile__img border fr-p-3v fr-m-0"
-        >
-          <img
-            :src="organization.logo"
-            loading="lazy"
-            class="fr-responsive-img"
-          />
+  <RouterLink
+    class="fr-tile__link"
+    :to="`/organizations/${organization.slug}` || ''"
+  >
+    <div class="fr-tile-v2 border">
+      <div class="fr-grid-row fr-grid-row--middle fr-mb-8v">
+        <div class="fr-col-auto">
+          <div
+            v-if="organization.logo"
+            class="fr-tile__img border fr-p-3v fr-m-0"
+          >
+            <img
+              :src="organization.logo"
+              loading="lazy"
+              class="fr-responsive-img"
+            />
+          </div>
+        </div>
+        <div class="fr-col fr-px-3v">
+          <h4 class="fr-title-v2__title">
+            <VIcon
+              v-if="isPublicService()"
+              name="ri-bank-line"
+              class="fr-mr-1v badge"
+            />
+            {{ organization.name }}
+            <VIcon
+              v-if="isCertified()"
+              name="ri-checkbox-circle-line"
+              class="fr-ml-1v badge"
+            />
+          </h4>
         </div>
       </div>
-      <div class="fr-col fr-px-3v">
-        <h4 class="fr-title-v2__title">
-          <VIcon
-            v-if="isPublicService()"
-            name="ri-bank-line"
-            class="fr-mr-1v badge"
+
+      <div v-if="organization.description" class="fr-tile-v2__body">
+        <p class="fr-tile__desc">
+          <text-clamp
+            :auto-resize="true"
+            :text="stripFromMarkdown(organization.description)"
+            :max-lines="3"
           />
-          <RouterLink
-            class="fr-tile__link"
-            :to="`/organizations/${organization.slug}` || ''"
+        </p>
+      </div>
+      <div>
+        <DsfrTag class="fr-card__detail fr-mt-1w fr-mb-1w card__tag">
+          <span
+            ><strong>{{ organization.metrics.datasets }}</strong> jeux de
+            données</span
           >
-            {{ organization.name }}
-          </RouterLink>
-          <VIcon
-            v-if="isCertified()"
-            name="ri-checkbox-circle-line"
-            class="fr-ml-1v badge"
-          />
-        </h4>
+        </DsfrTag>
       </div>
     </div>
-
-    <div v-if="organization.description" class="fr-tile-v2__body">
-      <p class="fr-tile__desc">
-        <text-clamp
-          :auto-resize="true"
-          :text="stripFromMarkdown(organization.description)"
-          :max-lines="3"
-        />
-      </p>
-    </div>
-    <div>
-      <DsfrTag class="fr-card__detail fr-mt-1w fr-mb-1w card__tag">
-        <span
-          ><strong>{{ organization.metrics.datasets }}</strong> jeux de
-          données</span
-        >
-      </DsfrTag>
-    </div>
-  </div>
+  </RouterLink>
 </template>
 
 <style scoped>
