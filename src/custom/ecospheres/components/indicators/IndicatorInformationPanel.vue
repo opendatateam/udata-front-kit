85d4395372f7b7cb43050ab066d08bf6abf0b1e3
Alexandre Bulté <alexandre@bulte.net>
2025-01-07 13:45:33 +0100

    indicator: IndicatorInformationPanel

---
+++ src/custom/ecospheres/components/indicators/IndicatorInformationPanel.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- TODO: split in subcomponents with slots, markup mayhem -->
+  <div class="fr-py-3w fr-mb-3w border-bottom border-default-grey">
+    <h2 class="subtitle subtitle--uppercase">Informations étendues</h2>
+    <div class="fr-text--sm fr-m-0">
+      <div class="fr-grid-row fr-grid-row--gutters">
+        <div class="fr-col-12 fr-col-sm-6 fr-col-md-4">
+          <h3 class="subtitle fr-mb-1v">Conditions d'accès et d'utilisation</h3>
+          <p class="fr-text--sm fr-m-0 text-mention-grey">Coucou</p>
+        </div>
+        <div class="fr-col-12 fr-col-sm-6 fr-col-md-4">
+          <h3 class="subtitle fr-mb-1v">Point de contact</h3>
+          <p class="fr-text--sm fr-m-0 text-mention-grey">
+            alexandre@datagouv.fr
+          </p>
+        </div>
+        <div class="fr-col-12 fr-col-sm-6 fr-col-md-4">
+          <h3 class="subtitle fr-mb-1v">Généalogie</h3>
+          <p class="fr-text--sm fr-m-0 text-mention-grey">Old</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/custom/ecospheres/views/indicators/IndicatorDetailView.vue
+++ src/custom/ecospheres/views/indicators/IndicatorDetailView.vue
@@ -16,6 +16,7 @@ import { useDatasetStore } from '@/store/DatasetStore'
 import { useUserStore } from '@/store/UserStore'
 import { descriptionFromMarkdown, formatDate } from '@/utils'
 import { useLicense } from '@/utils/dataset'
+import IndicatorInformationPanel from '../../components/indicators/IndicatorInformationPanel.vue'
 
 const route = useRouteParamsAsString()
 const indicatorId = route.params.iid
@@ -106,29 +107,7 @@ onMounted(() => {
     >
       <!-- Informations -->
       <DsfrTabContent panel-id="tab-content-0" tab-id="tab-0">
-        <div class="fr-py-3w fr-mb-3w border-bottom border-default-grey">
-          <h2 class="subtitle subtitle--uppercase">Informations étendues</h2>
-          <div class="fr-text--sm fr-m-0">
-            <div class="fr-grid-row fr-grid-row--gutters">
-              <div class="fr-col-12 fr-col-sm-6 fr-col-md-4">
-                <h3 class="subtitle fr-mb-1v">
-                  Conditions d'accès et d'utilisation
-                </h3>
-                <p class="fr-text--sm fr-m-0 text-mention-grey">Coucou</p>
-              </div>
-              <div class="fr-col-12 fr-col-sm-6 fr-col-md-4">
-                <h3 class="subtitle fr-mb-1v">Point de contact</h3>
-                <p class="fr-text--sm fr-m-0 text-mention-grey">
-                  alexandre@datagouv.fr
-                </p>
-              </div>
-              <div class="fr-col-12 fr-col-sm-6 fr-col-md-4">
-                <h3 class="subtitle fr-mb-1v">Généalogie</h3>
-                <p class="fr-text--sm fr-m-0 text-mention-grey">Old</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <IndicatorInformationPanel />
       </DsfrTabContent>
 
       <!-- Fichiers -->
