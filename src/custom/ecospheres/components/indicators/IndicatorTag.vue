822a4b6752d682cd69d4dbdb29436404f212c706
Alexandre Bulté <alexandre@bulte.net>
2025-01-07 17:40:14 +0100

    indicator: IndicatorTag

---
+++ src/custom/ecospheres/components/indicators/IndicatorTag.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { IndicatorTag } from '../../model/indicator'
+
+defineProps({
+  tag: {
+    type: Object as () => IndicatorTag,
+    default: undefined
+  }
+})
+</script>
+
+<template>
+  <p v-if="tag" :class="['fr-badge', `fr-badge--${tag.color}`]">
+    {{ tag.value }}
+  </p>
+  <p v-else class="fr-mb-0">-</p>
+</template>
