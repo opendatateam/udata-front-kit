0651adfa562e5c02bd83ccbab7f9d77f92c37fef
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2025-02-03 20:38:30 +0100

    rebase form mf

--- src/custom/meteo-france/routes.ts
+++ src/custom/meteo-france/routes.ts
@@ -2,9 +2,6 @@ export const routes = [
   {
     path: '/form',
     name: 'FormMF',
-    meta: {
-      title: 'Recherche guidée'
-    },
     component: async () => await import('./views/FormMF.vue')
   }
 ]
