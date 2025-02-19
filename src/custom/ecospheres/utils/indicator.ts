c83ff2dd009763ecf0100c86832f2b3f8331ff44
Alexandre Bulté <alexandre@bulte.net>
2025-02-19 15:03:28 +0100

    indicator: custom card in bouquet detail

--- src/custom/ecospheres/utils/indicator.ts
+++ src/custom/ecospheres/utils/indicator.ts
@@ -1,5 +1,6 @@
 import config from '@/config'
 import { useSpatialStore } from '@/store/SpatialStore'
+import type { DatasetV2 } from '@datagouv/components'
 import type { ComputedRef } from 'vue'
 import type { IndicatorsConf } from '../model/config'
 import type {
@@ -117,4 +118,15 @@ export const useIndicatorExtras = (indicator: Ref<Indicator | undefined>) => {
     api,
     sources
   }
+}
+
+// FIXME: use only tagPrefix when test data are OK
+export const isIndicator = (
+  dataset: Ref<DatasetV2 | undefined>
+): ComputedRef<boolean> => {
+  return computed(() => {
+    return (
+      dataset.value?.tags?.some((tag) => tag.startsWith(tagPrefix)) || false
+    )
+  })
 }
