27d04cd696a5b107c68edca0453bf7228e86cd6e
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-06-27 18:16:17 +0200

    use grist data from extras to display description

--- src/custom/simplifions/components/SimplifionsUseCaseDescription.vue
+++ src/custom/simplifions/components/SimplifionsUseCaseDescription.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>CUSTOM DESCRIPTION FROM GRIST</div>
+  <h1>{{ topic.name }}</h1>
+
+  <p class="fr-text--lead">{{ gris_data.Description_longue }}</p>
 </template>
+
+<script setup lang="ts">
+import type { Topic } from '@/model/topic'
+
+const props = defineProps<{
+  topic: Topic
+}>()
+
+const gris_data = (props.topic.extras as any)['cas-d-usages'] as Record<
+  string,
+  any
+>
+</script>
