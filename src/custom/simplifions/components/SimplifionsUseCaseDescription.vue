0429b06d8cc0ebc39ad5503e13d7c273f1c0f148
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-06-30 17:24:29 +0200

    display reco_solutions

--- src/custom/simplifions/components/SimplifionsUseCaseDescription.vue
+++ src/custom/simplifions/components/SimplifionsUseCaseDescription.vue
@@ -24,25 +24,22 @@
 
     <h2 class="fr-h2 fr-my-5w">Solutions disponibles</h2>
 
-    <h3 class="fr-h3 fr-my-3w">1. Sans développement, ni budget</h3>
+    <div
+      v-for="reco_solution in grist_data.reco_solutions"
+      :key="reco_solution.Nom_de_la_solution"
+    >
+      <SimplifionsRecoSolutions :reco_solution="reco_solution" />
+    </div>
 
-    <p>TODO</p>
+    <!-- <div v-for="(solutions, type_simplification) in grouped_reco_solutions" :key="type_simplification">
+      <h3 class="fr-h3 fr-mb-3w">{{ type_simplification }}</h3>
+      
+      <div v-for="reco_solution in solutions" :key="reco_solution.Nom_de_la_solution">
+        <SimplifionsRecoSolutions :reco_solution="reco_solution" />
+      </div>
+    </div> -->
   </div>
 </template>
-
-<script setup lang="ts">
-import type { Topic } from '@/model/topic'
-import { fromMarkdown } from '@/utils'
-
-const props = defineProps<{
-  topic: Topic
-}>()
-
-const grist_data = (props.topic.extras as any)['cas-d-usages'] as Record<
-  string,
-  any
->
-</script>
 
 <style scoped>
 h2 {
@@ -55,3 +52,27 @@ h3 {
   color: #616161;
 }
 </style>
+
+<script setup lang="ts">
+import type { Topic } from '@/model/topic'
+import { fromMarkdown } from '@/utils'
+import SimplifionsRecoSolutions from './SimplifionsRecoSolutions.vue'
+
+const props = defineProps<{
+  topic: Topic
+}>()
+
+const grist_data = (props.topic.extras as any)['cas-d-usages'] as Record<
+  string,
+  any
+>
+
+// const grouped_reco_solutions = grist_data.reco_solutions.reduce((acc: Record<string, any[]>, reco_solution: any) => {
+//   const key = reco_solution.Type_de_simplification
+//   if (!acc[key]) {
+//     acc[key] = []
+//   }
+//   acc[key].push(reco_solution)
+//   return acc
+// }, {} as Record<string, any[]>)
+</script>
