b099bdba349e53867c42d5fd758e143a209474f8
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-06-30 17:59:39 +0200

    config for simplifions

---
+++ src/custom/simplifions/routes.ts
@@ -0,0 +1,39 @@
+import {
+  useDatasetSearchPageRoutes,
+  useTopicAdminPagesRoutes,
+  useTopicSearchPageRoutes
+} from '@/router/utils'
+import type { RouteRecordRaw } from 'vue-router'
+
+const topicConf = {
+  displayMetadata: false,
+  enableReadMore: false,
+  datasetEditorialization: false
+}
+
+export const routes: RouteRecordRaw[] = [
+  useDatasetSearchPageRoutes({
+    pageKey: 'datasets',
+    metaTitle: 'Données'
+  }),
+  useTopicSearchPageRoutes({
+    pageKey: 'cas-d-usages',
+    metaTitle: "Cas d'usages",
+    topicConf,
+    cardComponent: async () => await import('./components/CustomTopicCard.vue')
+  }),
+  ...useTopicAdminPagesRoutes({
+    pageKey: 'cas-d-usages',
+    topicConf
+  }),
+  useTopicSearchPageRoutes({
+    pageKey: 'solutions',
+    metaTitle: 'Solutions',
+    topicConf,
+    cardComponent: async () => await import('./components/CustomTopicCard.vue')
+  }),
+  ...useTopicAdminPagesRoutes({
+    pageKey: 'solutions',
+    topicConf
+  })
+]
