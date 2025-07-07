b5405e44cdce875b5dcc8f6f668db5873b243612
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-07-07 13:23:37 +0200

    conditions around reco solution elements

--- src/custom/simplifions/components/SimplifionsRecoSolutions.vue
+++ src/custom/simplifions/components/SimplifionsRecoSolutions.vue
@@ -28,7 +28,11 @@
       </div>
 
       <div class="fr-col-12 fr-col-md-8">
-        <p>
+        <p
+          v-if="
+            reco_solution.En_quoi_cette_solution_est_elle_utile_pour_ce_cas_d_usage_
+          "
+        >
           <strong>
             <span
               aria-hidden="true"
@@ -43,19 +47,19 @@
           }}
         </p>
 
-        <p>
+        <p v-if="reco_solution.Concretement_pour_les_usagers_">
           <strong>🧑 Concrètement, pour les usagers :</strong>
           <br />
           {{ reco_solution.Concretement_pour_les_usagers_ }}
         </p>
 
-        <p>
+        <p v-if="reco_solution.Concretement_pour_vos_agents_">
           <strong>🧑‍💼 Concrètement, pour vos agents :</strong>
           <br />
           {{ reco_solution.Concretement_pour_vos_agents_ }}
         </p>
 
-        <p>
+        <p v-if="reco_solution.Ce_que_ne_fait_pas_cette_solution_">
           <strong>
             <span
               aria-hidden="true"
