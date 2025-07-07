457b44290e37d128d300dd958a83cca0f2796098
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-07-07 11:57:56 +0200

    grouped reco solutions

--- src/custom/simplifions/components/SimplifionsCasDusageDescription.vue
+++ src/custom/simplifions/components/SimplifionsCasDusageDescription.vue
@@ -45,20 +45,15 @@
                 >Solutions disponibles</a
               >
               <ol>
-                <li>
+                <li
+                  v-for="(title, index) in Object.keys(grouped_reco_solutions)"
+                  :key="title"
+                >
                   <a
-                    href="#solution-sans-dev-ni-budget"
+                    :href="`#solution-${index + 1}`"
                     id="summary-link-2-1"
                     class="fr-summary__link"
-                    >Sans développement, ni budget</a
-                  >
-                </li>
-                <li>
-                  <a
-                    href="#solution-dev-ou-editeur"
-                    id="summary-link-2-2"
-                    class="fr-summary__link"
-                    >Avec des moyens techniques ou un éditeur de logiciel</a
+                    >{{ title }}</a
                   >
                 </li>
               </ol>
@@ -87,19 +82,18 @@
     <h2 class="fr-h2 fr-my-5w">Solutions disponibles</h2>
 
     <div
-      v-for="reco_solution in casUsage.reco_solutions"
-      :key="reco_solution.Nom_de_la_solution"
+      v-for="(title, index) in Object.keys(grouped_reco_solutions)"
+      :key="title"
     >
-      <SimplifionsRecoSolutions :reco_solution="reco_solution" />
-    </div>
+      <h3 class="fr-h3 fr-mb-3w">{{ index + 1 }}. {{ title }}</h3>
 
-    <!-- <div v-for="(solutions, type_simplification) in grouped_reco_solutions" :key="type_simplification">
-      <h3 class="fr-h3 fr-mb-3w">{{ type_simplification }}</h3>
-      
-      <div v-for="reco_solution in solutions" :key="reco_solution.Nom_de_la_solution">
+      <div
+        v-for="reco_solution in grouped_reco_solutions[title]"
+        :key="reco_solution.Nom_de_la_solution"
+      >
         <SimplifionsRecoSolutions :reco_solution="reco_solution" />
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
@@ -135,12 +129,22 @@ const casUsage = (props.topic.extras as any)[
   'simplifions-cas-d-usages'
 ] as Record<string, any>
 
-// const grouped_reco_solutions = casUsage.reco_solutions.reduce((acc: Record<string, any[]>, reco_solution: any) => {
-//   const key = reco_solution.Type_de_simplification
-//   if (!acc[key]) {
-//     acc[key] = []
-//   }
-//   acc[key].push(reco_solution)
-//   return acc
-// }, {} as Record<string, any[]>)
+const budget_group = (budget: Array<string>) => {
+  if (budget.includes('Aucun développement, ni budget')) {
+    return 'Aucun développement, ni budget'
+  }
+  return 'Avec des moyens techniques ou un éditeur de logiciel'
+}
+
+const grouped_reco_solutions = casUsage.reco_solutions.reduce(
+  (acc: Record<string, any[]>, reco_solution: any) => {
+    const key = budget_group(reco_solution.Moyens_requis_pour_la_mise_en_oeuvre)
+    if (!acc[key]) {
+      acc[key] = []
+    }
+    acc[key].push(reco_solution)
+    return acc
+  },
+  {} as Record<string, any[]>
+)
 </script>
