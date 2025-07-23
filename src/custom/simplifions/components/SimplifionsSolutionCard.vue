38731d5f0e1ba466ce85688aa47a2c87087f0db1
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-07-23 15:00:21 +0200

    use router-link in all simplifions components

--- src/custom/simplifions/components/SimplifionsSolutionCard.vue
+++ src/custom/simplifions/components/SimplifionsSolutionCard.vue
@@ -1,5 +1,7 @@
 <template>
-  <RouterLink :to="topicLink">
+  <router-link
+    :to="{ name: `${pageKey}_detail`, params: { item_id: props.topic.slug } }"
+  >
     <div class="fr-card fr-enlarge-link fr-card--shadow topic-card">
       <div class="fr-card__body">
         <div class="fr-card__content">
@@ -39,14 +41,13 @@
         </div>
       </div>
     </div>
-  </RouterLink>
+  </router-link>
 </template>
 
 <script setup lang="ts">
 import type { Topic } from '@/model/topic'
 import { useCurrentPageConf } from '@/router/utils'
 import { stripFromMarkdown } from '@/utils'
-import type { RouteLocationRaw } from 'vue-router'
 import type { SimplifionsSolutionsExtras } from '../model/solution'
 import { gristImageUrl } from './simplifions_utils'
 import SimplifionsTags from './SimplifionsTags.vue'
@@ -67,11 +68,6 @@ const solution = (props.topic.extras as SimplifionsSolutionsExtras)[
 const imageUrl = solution?.Image_principale?.[0]
   ? gristImageUrl(solution.Image_principale[0])
   : ''
-
-const topicLink: RouteLocationRaw = {
-  name: `${pageKey}_detail`,
-  params: { item_id: props.topic.slug }
-}
 </script>
 
 <style scoped>
