843e09cec0f2f70f29856c7f9579def71f028b5b
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2025-07-29 14:33:11 +0200

    feat: add culture portal

---
+++ src/custom/culture/routes.ts
@@ -0,0 +1,23 @@
+import { useDatasetSearchPageRoutes } from '@/router/utils'
+import type { RouteRecordRaw } from 'vue-router'
+
+const topicConf = {
+  displayMetadata: false,
+  enableReadMore: false,
+  datasetEditorialization: false
+}
+
+export const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    meta: {
+      title: 'Accueil'
+    },
+    component: async () => await import('./views/HomeView.vue')
+  },
+  useDatasetSearchPageRoutes({
+    pageKey: 'datasets',
+    metaTitle: 'Données'
+  })
+]
---
+++ src/custom/culture/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="fr-container">Coucou</div>
+</template>
+
+<style scoped></style>
