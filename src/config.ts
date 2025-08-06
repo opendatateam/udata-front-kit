63d3ffb0ce3897c8fcebcd16b87abcef699d3fa5
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-08-06 14:18:16 +0200

    dirty but functional way to import and merge mode-specific configs

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,31 @@
 import config from '@siteConfig/config.yaml'
 
-export default config
+const mode = import.meta.env.MODE
+const site_id = import.meta.env.VITE_SITE_ID
+
+// Use Vite's import.meta.glob to conditionally load mode-specific configs
+const modeConfigs = import.meta.glob('@siteConfig/config.*.yaml', {
+  eager: true
+})
+
+console.log(modeConfigs)
+
+// Try to find and merge mode-specific config
+let finalConfig = config
+const modeConfigKey = `/configs/${site_id}/config.${mode}.yaml`
+
+if (modeConfigs[modeConfigKey]) {
+  console.log(
+    `Loading and merging config for ${mode} with the default config of ${site_id}`
+  )
+  const configForMode = modeConfigs[modeConfigKey] as any
+  finalConfig = { ...config, ...configForMode.default }
+} else {
+  console.log(
+    `No specific config found for ${mode}, using default config of ${site_id}`
+  )
+}
+
+console.log(finalConfig)
+
+export default finalConfig
