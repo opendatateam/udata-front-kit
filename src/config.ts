45d3ee166d9df6559bd3bc49f96435a4c0a70483
Alexandre Bulté <alexandre@bulte.net>
2025-08-06 19:55:09 +0200

    comment

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+// __APP_CONFIG__ is exported from {site_id}/config.*.yaml files in vite.config.mts
 // TODO: we should type the entire config here
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const __APP_CONFIG__: any
