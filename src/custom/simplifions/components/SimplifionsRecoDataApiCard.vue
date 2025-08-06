39a14081b35fb206290107b3c941847ab31170a1
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-08-06 17:17:59 +0200

    display description in markdown

--- src/custom/simplifions/components/SimplifionsRecoDataApiCard.vue
+++ src/custom/simplifions/components/SimplifionsRecoDataApiCard.vue
@@ -6,10 +6,17 @@
       >{{ apiOrData.Nom_donnees_ou_API }}</a
     >
     <p v-if="customDescription">{{ customDescription }}</p>
+    <!-- eslint-disable vue/no-v-html -->
+    <div
+      v-if="customDescription && customDescription.length > 0"
+      v-html="fromMarkdown(customDescription)"
+    ></div>
+    <!-- eslint-enable vue/no-v-html -->
   </div>
 </template>
 
 <script setup lang="ts">
+import { fromMarkdown } from '@/utils'
 import type { SimplifionsDataOrApi } from '../model/cas_usage'
 
 defineProps<{
