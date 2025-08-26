ce8bdf8b1e81c74938cff838c70d30e2a258ccce
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-08-26 15:28:45 +0200

    improve datagouv request interception

--- cypress/support/datagouv_mocks.js
+++ cypress/support/datagouv_mocks.js
@@ -11,25 +11,29 @@ const datagouvResponseBuilder = (data) => {
   }
 }
 
-Cypress.Commands.add('mockDatagouvResource', (resource_name, data) => {
-  cy.intercept('GET', `**/api/2/${resource_name}/**`, {
+const datagouvUrlRegex = (resourceName) => {
+  return new RegExp(`.*data\\.gouv\\.fr/api/2/${resourceName}.*`)
+}
+
+Cypress.Commands.add('mockDatagouvResource', (resourceName, data) => {
+  cy.intercept('GET', datagouvUrlRegex(resourceName), {
     statusCode: 200,
     body: datagouvResponseBuilder(data)
-  }).as(`get${capitalizeFirstLetter(resource_name)}`)
+  }).as(`get${capitalizeFirstLetter(resourceName)}`)
 })
 
 // We can't use req.query to check the request params because the tags are not properly handled as an array
 // in the request so req.query only contains the last tag. I ended up checking for the params in the url instead.
 Cypress.Commands.add(
   'expectRequestWithParams',
-  (resource_name, request_params_string) => {
-    cy.intercept('GET', `**/api/2/${resource_name}/**`, (req) => {
-      if (request_params_string instanceof RegExp) {
-        expect(req.url).to.match(request_params_string)
+  (resourceName, requestParamsString) => {
+    cy.intercept('GET', datagouvUrlRegex(resourceName), (req) => {
+      if (requestParamsString instanceof RegExp) {
+        expect(req.url).to.match(requestParamsString)
       } else {
-        expect(req.url).to.include(request_params_string)
+        expect(req.url).to.include(requestParamsString)
       }
-    }).as(`get${capitalizeFirstLetter(resource_name)}`)
+    }).as(`get${capitalizeFirstLetter(resourceName)}`)
   }
 )
 
