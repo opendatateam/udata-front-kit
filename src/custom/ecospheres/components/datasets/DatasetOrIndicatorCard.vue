721185fd556f1ec8d647c1f7ff6d50bde6f545f0
Alexandre Bulté <alexandre@bulte.net>
2026-01-15 17:58:30 +0100

    change strategy re card again

--- src/custom/ecospheres/components/datasets/DatasetOrIndicatorCard.vue
+++ src/custom/ecospheres/components/datasets/DatasetOrIndicatorCard.vue
@@ -3,7 +3,6 @@ import { isIndicator } from '@/custom/ecospheres/utils/indicator'
 import type { DatasetV2 } from '@datagouv/components-next'
 import { DatasetCard } from '@datagouv/components-next'
 import { toRef } from 'vue'
-import IndicatorInDatasetListCard from './IndicatorInDatasetListCard.vue'
 
 const props = defineProps<{
   dataset: DatasetV2
@@ -13,25 +12,43 @@ const props = defineProps<{
 
 const datasetIsIndicator = isIndicator(toRef(props, 'dataset'))
 
-const indicatorUrl = computed(() => ({
-  name: 'indicators_detail',
-  params: { item_id: props.dataset.id }
-}))
+const url = computed(() =>
+  datasetIsIndicator.value
+    ? { name: 'indicators_detail', params: { item_id: props.dataset.id } }
+    : props.datasetUrl
+)
 </script>
 
 <template>
-  <IndicatorInDatasetListCard
-    v-if="datasetIsIndicator"
-    :key="dataset.id"
-    :dataset="dataset"
-    :dataset-url="indicatorUrl"
-    :organization-url="organizationUrl"
-  />
-  <DatasetCard
-    v-else
-    :key="dataset.id"
-    :dataset="dataset"
-    :dataset-url="datasetUrl"
-    :organization-url="organizationUrl"
-  />
+  <div :class="{ 'indicator-card-wrapper': datasetIsIndicator }">
+    <p
+      v-if="datasetIsIndicator"
+      class="fr-badge fr-badge--sm fr-badge--mention-grey text-grey-380 indicator-badge"
+    >
+      <span
+        class="fr-icon-lightbulb-line fr-icon--sm"
+        aria-hidden="true"
+      ></span>
+      Indicateur
+    </p>
+    <DatasetCard
+      :dataset="dataset"
+      :dataset-url="url"
+      :organization-url="organizationUrl"
+    />
+  </div>
 </template>
+
+<style scoped>
+.indicator-card-wrapper {
+  position: relative;
+}
+
+.indicator-badge {
+  position: absolute;
+  top: 0;
+  left: 0.5rem;
+  transform: translateY(-50%);
+  z-index: 1;
+}
+</style>
