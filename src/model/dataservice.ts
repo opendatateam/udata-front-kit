85691a20f0fb6bf1ee0c4ca6c3b69ee8c10367ac
Alexandre Bulté <alexandre@bulte.net>
2026-02-09 16:20:07 +0100

    feat(datasets): reuses and api tab

--- src/model/dataservice.ts
+++ src/model/dataservice.ts
@@ -1,14 +1,7 @@
-import type { Dataservice, Owned, Rel } from '@datagouv/components-next'
+import type { Dataservice } from '@datagouv/components-next'
 
 import type { GenericResponse } from './api'
 
-// TODO: this should probably be fixed upstream as main Dataservice type
-// Using Owned explicitly to preserve the discriminated union
-export type DataserviceWithRel = Owned &
-  Omit<Dataservice, 'datasets' | 'organization' | 'owner'> & {
-    datasets: Rel
-  }
-
 export interface DataserviceResponse extends GenericResponse {
-  data: DataserviceWithRel[]
+  data: Dataservice[]
 }
