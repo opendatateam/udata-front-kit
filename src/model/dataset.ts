794ed6372b16b5fcccf8b12ecbf3bddd22ae3aaa
Alexandre Bulté <alexandre@bulte.net>
2026-02-09 14:38:16 +0100

    comment overloaded types

--- src/model/dataset.ts
+++ src/model/dataset.ts
@@ -3,6 +3,8 @@ import type { DatasetV2, Harvest } from '@datagouv/components-next'
 import type { GenericResponse } from './api'
 import type { ResolvedFactor } from './topic'
 
+// FIXME: upstream type is Record<string, unknown> for harvest keys (except `backend`)
+// we set our own precise types for attributes we need, `unknown` being problematic
 export type TypedHarvest = Harvest & {
   uri?: string
   remote_url?: string
