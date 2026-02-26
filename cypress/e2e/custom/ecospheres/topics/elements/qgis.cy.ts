6e1e6351269c3bc7c3797a22e25124d208186db3
Alexandre Bulté <alexandre@bulte.net>
2026-02-26 16:46:36 +0100

    feat(dataset): add QGIS export on dataset page

--- cypress/e2e/custom/ecospheres/topics/elements/qgis.cy.ts
+++ cypress/e2e/custom/ecospheres/topics/elements/qgis.cy.ts
@@ -99,7 +99,7 @@ describe('Topic Elements - QGIS Integration', () => {
       cy.get('@createObjectURL').should('have.been.calledOnce')
     })
 
-    it('should keep individual dataset QGIS buttons alongside topic-level button', () => {
+    it('should NOT show individual dataset QGIS button in topic (button moved to dataset page)', () => {
       // Create single factor with OGC resource
       testFactor = factorFactory.one({ traits: ['dataset_in_group'] })
       testTopic = createTestTopicWithElements([testFactor])
@@ -117,140 +117,11 @@ describe('Topic Elements - QGIS Integration', () => {
       visitTopic(testTopic.slug)
       cy.wait('@getElementsDataset')
 
-      // Verify topic-level button exists
-      cy.get('.test__open_topic_in_qgis_btn').should('be.visible')
-
       // Expand group to see individual dataset
       expandDisclosureGroup()
 
-      // Verify individual dataset button also exists
-      cy.get('.test__open_dataset_in_qgis_btn').should('be.visible')
-    })
-  })
-
-  describe('QGIS Button Display', () => {
-    it('should show "Ouvrir dans QGIS" button for dataset with WFS service', () => {
-      // Create a factor with a dataset
-      testFactor = factorFactory.one({ traits: ['dataset_in_group'] })
-      testTopic = createTestTopicWithElements([testFactor])
-
-      const datasetId = testFactor.element!.id
-
-      // Create WFS resource
-      const wfsResource = resourceFactory.one({ traits: ['wfs'] })
-
-      // Mock topic and dataset with WFS resource
-      mockTopicAndRelatedObjects(testTopic, {
-        factors: [testFactor],
-        datasetResources: {
-          [datasetId]: [wfsResource]
-        }
-      })
-      mockTopicElementsByClass(testTopic.id, [testFactor], [], [])
-
-      visitTopic(testTopic.slug)
-      cy.wait('@getElementsDataset')
-
-      // Expand group to see the dataset
-      expandDisclosureGroup()
-
-      // Verify QGIS button is visible
-      cy.get('.test__open_dataset_in_qgis_btn').should('be.visible')
-    })
-
-    it('should show "Ouvrir dans QGIS" button for dataset with WMS service', () => {
-      // Create a factor with a dataset
-      testFactor = factorFactory.one({ traits: ['dataset_in_group'] })
-      testTopic = createTestTopicWithElements([testFactor])
-
-      const datasetId = testFactor.element!.id
-
-      // Create WMS resource
-      const wmsResource = resourceFactory.one({ traits: ['wms'] })
-
-      // Mock topic and dataset with WMS resource
-      mockTopicAndRelatedObjects(testTopic, {
-        factors: [testFactor],
-        datasetResources: {
-          [datasetId]: [wmsResource]
-        }
-      })
-      mockTopicElementsByClass(testTopic.id, [testFactor], [], [])
-
-      visitTopic(testTopic.slug)
-      cy.wait('@getElementsDataset')
-
-      // Expand group to see the dataset
-      expandDisclosureGroup()
-
-      // Verify QGIS button is visible
-      cy.get('.test__open_dataset_in_qgis_btn').should('be.visible')
-    })
-
-    it('should NOT show "Ouvrir dans QGIS" button for dataset without OGC service', () => {
-      // Create a factor with a dataset
-      testFactor = factorFactory.one({ traits: ['dataset_in_group'] })
-      testTopic = createTestTopicWithElements([testFactor])
-
-      const datasetId = testFactor.element!.id
-
-      // Create regular CSV resource (no OGC service)
-      const csvResource = resourceFactory.one({ traits: ['csv'] })
-
-      // Mock topic and dataset with CSV resource only
-      mockTopicAndRelatedObjects(testTopic, {
-        factors: [testFactor],
-        datasetResources: {
-          [datasetId]: [csvResource]
-        }
-      })
-      mockTopicElementsByClass(testTopic.id, [testFactor], [], [])
-
-      visitTopic(testTopic.slug)
-      cy.wait('@getElementsDataset')
-
-      // Expand group to see the dataset
-      expandDisclosureGroup()
-
-      // Verify QGIS button does NOT exist
+      // Verify individual dataset button does NOT exist in topic view
       cy.get('.test__open_dataset_in_qgis_btn').should('not.exist')
-    })
-
-    it('should trigger download when clicking "Ouvrir dans QGIS" button', () => {
-      // Create a factor with a dataset
-      testFactor = factorFactory.one({ traits: ['dataset_in_group'] })
-      testTopic = createTestTopicWithElements([testFactor])
-
-      const datasetId = testFactor.element!.id
-
-      // Create WFS resource
-      const wfsResource = resourceFactory.one({ traits: ['wfs'] })
-
-      // Mock topic and dataset with WFS resource
-      mockTopicAndRelatedObjects(testTopic, {
-        factors: [testFactor],
-        datasetResources: {
-          [datasetId]: [wfsResource]
-        }
-      })
-      mockTopicElementsByClass(testTopic.id, [testFactor], [], [])
-
-      visitTopic(testTopic.slug)
-      cy.wait('@getElementsDataset')
-
-      // Expand group to see the dataset
-      expandDisclosureGroup()
-
-      // Spy on URL.createObjectURL to detect blob creation
-      cy.window().then((win) => {
-        cy.spy(win.URL, 'createObjectURL').as('createObjectURL')
-      })
-
-      // Click the QGIS button
-      cy.get('.test__open_dataset_in_qgis_btn').click()
-
-      // Verify that URL.createObjectURL was called (indicating download triggered)
-      cy.get('@createObjectURL').should('have.been.calledOnce')
     })
   })
 })
