bdbe53129466ae544a68833e05a801372aa083ab
Alexandre Bulté <alexandre@bulte.net>
2026-04-28 17:32:54 +0200

    simpler SearchOrganizationFilter

--- src/components/search/SearchOrganizationFilter.vue
+++ src/components/search/SearchOrganizationFilter.vue
@@ -1,54 +1,34 @@
 <script setup lang="ts">
+import SearchSelectFilter from '@/components/search/SearchSelectFilter.vue'
 import type { OrganizationFilterConfig } from '@/router/utils'
 import { useOrganizationStore } from '@/store/OrganizationStore'
-import { SearchableSelect, useSearchFilter } from '@datagouv/components-next'
 import { computed, onMounted, ref, watch } from 'vue'
 
 const props = defineProps<{ config: OrganizationFilterConfig }>()
 
 const organizationStore = useOrganizationStore()
-
-const orgOptions = ref<{ id: string; name: string }[]>([])
+const orgOptions = ref<{ value: string; label: string }[]>([])
 
 const loadOrgs = async (key: string) => {
   const orgs = await organizationStore.loadFromConfigFlat(key)
-  orgOptions.value = orgs.map(({ id, name }) => ({ id, name }))
+  orgOptions.value = orgs.map(({ id, name }) => ({ value: id, label: name }))
 }
 
-onMounted(async () => {
-  await loadOrgs(props.config.pageKey)
-})
-
+onMounted(() => loadOrgs(props.config.pageKey))
 watch(
   () => props.config.pageKey,
-  async (newKey) => {
-    await loadOrgs(newKey)
-  }
+  (newKey) => loadOrgs(newKey)
 )
 
-const urlValue = useSearchFilter('organization', {
-  apiParam: 'organization'
-})
-
-const model = computed<{ id: string; name: string } | null>({
-  get: () =>
-    urlValue.value
-      ? (orgOptions.value.find((o) => o.id === urlValue.value) ?? null)
-      : null,
-  set: (opt) => {
-    urlValue.value = opt?.id ?? undefined
-  }
-})
+const selectConfig = computed(() => ({
+  urlParam: 'organization',
+  apiParam: 'organization',
+  label: props.config.label,
+  defaultLabel: props.config.defaultLabel,
+  values: orgOptions.value
+}))
 </script>
 
 <template>
-  <SearchableSelect
-    v-model="model"
-    :options="orgOptions"
-    :get-option-id="(opt) => opt.id"
-    :display-value="(opt) => opt?.name ?? ''"
-    :placeholder="config.defaultLabel"
-    :label="config.label"
-    :multiple="false"
-  />
+  <SearchSelectFilter :config="selectConfig" />
 </template>
