1fc3123ce33c90906e1feed6a3f75a74ff105ffc
Alexandre Bulté <alexandre@bulte.net>
2026-05-06 09:45:18 +0200

    chore: upgrade cypress to 15.14.2

--- cypress/support/grist_mocks.js
+++ cypress/support/grist_mocks.js
@@ -2,7 +2,7 @@ Cypress.Commands.add('mockGristImages', () => {
   cy.readFile('public/static/blank_state/file.svg').then((svgContent) => {
     cy.intercept(
       'GET',
-      'https://grist.numerique.gouv.fr/api/docs/*/attachments/*/download',
+      /^https:\/\/grist\.numerique\.gouv\.fr\/api\/docs\/[^/]+\/attachments\/[^/]+\/download/,
       {
         statusCode: 200,
         headers: {
@@ -15,36 +15,35 @@ Cypress.Commands.add('mockGristImages', () => {
 })
 
 Cypress.Commands.add('mockGristRecords', (resourceId, elements = []) => {
-  cy.intercept(
-    'GET',
-    `https://grist.numerique.gouv.fr/api/docs/*/tables/${resourceId}/records*`,
-    (req) => {
-      const url = new URL(req.url)
-      const filterParam = url.searchParams.get('filter')
-      const filter = filterParam ? JSON.parse(filterParam) : {}
+  const tablePattern = resourceId === '*' ? '[^/]+' : resourceId
+  const urlRegex = new RegExp(
+    `^https://grist\\.numerique\\.gouv\\.fr/api/docs/[^/]+/tables/${tablePattern}/records`
+  )
+  cy.intercept('GET', urlRegex, (req) => {
+    const url = new URL(req.url)
+    const filterParam = url.searchParams.get('filter')
+    const filter = filterParam ? JSON.parse(filterParam) : {}
 
-      let records = elements
-      if (filter.id) {
-        records = elements.filter((el) => filter.id.includes(el.id))
-      }
-
-      req.reply({ statusCode: 200, body: { records } })
+    let records = elements
+    if (filter.id) {
+      records = elements.filter((el) => filter.id.includes(el.id))
     }
-  ).as(`get_grist_${resourceId}_records`)
+
+    req.reply({ statusCode: 200, body: { records } })
+  }).as(`get_grist_${resourceId}_records`)
 })
 
 Cypress.Commands.add('mockGristRecordsByIds', (resourceId, elements) => {
   const ids = elements.map((el) => el.id)
-  cy.intercept(
-    'GET',
-    `https://grist.numerique.gouv.fr/api/docs/*/tables/${resourceId}/records?filter=%7B%22id%22:%5B${ids.join(',')}%5D%7D`,
-    {
-      statusCode: 200,
-      body: {
-        records: elements
-      }
+  const urlRegex = new RegExp(
+    `^https://grist\\.numerique\\.gouv\\.fr/api/docs/[^/]+/tables/${resourceId}/records\\?filter=%7B%22id%22:%5B${ids.join(',')}%5D%7D$`
+  )
+  cy.intercept('GET', urlRegex, {
+    statusCode: 200,
+    body: {
+      records: elements
     }
-  ).as(`get_grist_${resourceId}_records_${ids.join('_')}`)
+  }).as(`get_grist_${resourceId}_records_${ids.join('_')}`)
 })
 
 Cypress.Commands.add('mockGristRecord', (resourceId, element) => {
