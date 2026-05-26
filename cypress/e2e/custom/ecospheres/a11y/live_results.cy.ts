f0f3cacb371e9cc563a527e82676e4c9f8aec9e4
Alexandre Bulté <alexandre@bulte.net>
2026-05-26 16:55:02 +0200

    add e2e

---
+++ cypress/e2e/custom/ecospheres/a11y/live_results.cy.ts
@@ -0,0 +1,56 @@
+import { datasetFactory } from 'cypress/support/factories/datasets_factory'
+
+describe('Unified search – live result count announcement', () => {
+  beforeEach(() => {
+    cy.mockMatomo()
+    cy.mockStaticDatagouv()
+  })
+
+  it('announces multiple results as "N résultats"', () => {
+    cy.mockListApis('datasets', datasetFactory.many(5))
+    cy.visit('/datasets')
+    cy.wait('@get_datasets_list')
+
+    cy.get('[aria-live="assertive"]')
+      .find('li')
+      .last()
+      .should('have.text', '5 résultats')
+  })
+
+  it('announces a single result as "1 résultat"', () => {
+    cy.mockListApis('datasets', datasetFactory.many(1))
+    cy.visit('/datasets')
+    cy.wait('@get_datasets_list')
+
+    cy.get('[aria-live="assertive"]')
+      .find('li')
+      .last()
+      .should('have.text', '1 résultat')
+  })
+
+  it('announces "Aucun résultat" when there are no results', () => {
+    cy.mockListApis('datasets', [])
+    cy.visit('/datasets')
+    cy.wait('@get_datasets_list')
+
+    cy.get('[aria-live="assertive"]')
+      .find('li')
+      .last()
+      .should('have.text', 'Aucun résultat')
+  })
+
+  it('includes the search query in the live-region page title', () => {
+    // pageTitle computed: "${page.title} pour \"${q}\"" when q is present
+    cy.mockListApis('datasets', datasetFactory.many(2))
+    cy.visit('/datasets?q=test')
+    cy.wait('@get_datasets_list')
+
+    const { pages, website } = Cypress.env('siteConfig')
+    const pageTitle = pages.datasets.title
+
+    cy.get('[aria-live="assertive"]')
+      .find('li')
+      .first()
+      .should('have.text', `Page ${pageTitle} pour "test" | ${website.title}`)
+  })
+})
