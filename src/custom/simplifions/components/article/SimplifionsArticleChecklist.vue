029a304c23576c22d62d49407479da27f4548ac3
Dorine Lambinet <dorine.lambinet@free.fr>
2026-06-24 15:47:21 +0200

    Remplace l'icône SVG inline de la checklist par l'icône DSFR checkbox-circle-line

--- src/custom/simplifions/components/article/SimplifionsArticleChecklist.vue
+++ src/custom/simplifions/components/article/SimplifionsArticleChecklist.vue
@@ -33,8 +33,8 @@
   width: 1.25rem;
   height: 1.25rem;
   background-color: var(--text-default-success);
-  -webkit-mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6 7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z'/%3E%3C/svg%3E");
-  mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6 7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z'/%3E%3C/svg%3E");
+  -webkit-mask-image: url('@gouvfr/dsfr/dist/icons/system/checkbox-circle-line.svg');
+  mask-image: url('@gouvfr/dsfr/dist/icons/system/checkbox-circle-line.svg');
   -webkit-mask-size: 100% 100%;
   mask-size: 100% 100%;
 }
